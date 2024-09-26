62ed5b735443962fd70c2f28b7dc3dfc0e3b37bc
Terence Tao <tao@math.ucla.edu>
2024-09-26 14:09:53 -0700

    sort implications in numerically increasing order

--- equational_theories/Basic.lean
+++ equational_theories/Basic.lean
@@ -58,8 +58,8 @@ def Equation4582 (G: Type*) [Magma G] := ∀ x y z w u v: G, x ∘ (y ∘ z) = (
 
 /- Positive implications -/
 
-theorem Equation2_implies_Equation46 (G: Type*) [Magma G] (h: Equation2 G) : Equation46 G :=
-  fun _ _ _ _ => h _ _
+theorem Equation1_true (G: Type*) [Magma G] : Equation1 G :=
+  fun _ => rfl
 
 theorem Equation2_implies_Equation4 (G: Type*) [Magma G] (h: Equation2 G) : Equation4 G :=
   fun _ _ => h _ _
@@ -67,23 +67,17 @@ theorem Equation2_implies_Equation4 (G: Type*) [Magma G] (h: Equation2 G) : Equa
 theorem Equation2_implies_Equation6 (G: Type*) [Magma G] (h: Equation2 G) : Equation6 G :=
   sorry
 
-theorem Equation6_implies_Equation2 (G: Type*) [Magma G] (h: Equation6 G) : Equation2 G :=
+theorem Equation2_implies_Equation7 (G: Type*) [Magma G] (h: Equation2 G) : Equation7 G :=
   sorry
 
-theorem Equation2_implies_Equation7 (G: Type*) [Magma G] (h: Equation2 G) : Equation7 G :=
+theorem Equation2_implies_Equation46 (G: Type*) [Magma G] (h: Equation2 G) : Equation46 G :=
+  fun _ _ _ _ => h _ _
+
+theorem Equation6_implies_Equation2 (G: Type*) [Magma G] (h: Equation6 G) : Equation2 G :=
   sorry
 
 theorem Equation7_implies_Equation2 (G: Type*) [Magma G] (h: Equation7 G) : Equation2 G :=
   sorry
-
-theorem Equation46_implies_Equation387 (G: Type*) [Magma G] (h: Equation46 G) : Equation387 G :=
-  fun _ _ => h _ _ _ _
-
-theorem Equation46_implies_Equation4582 (G: Type*) [Magma G] (h: Equation46 G) : Equation4582 G :=
-  fun _ _ _ _ _ _ => h _ _ _ _
-
-theorem Equation46_implies_Equation42 (G: Type*) [Magma G] (h: Equation46 G) : Equation42 G :=
-  fun _ _ _ => h _ _ _ _
 
 theorem Equation4_implies_Equation3 (G: Type*) [Magma G] (h: Equation4 G) : Equation3 G := by
   intro _
@@ -97,6 +91,15 @@ theorem Equation4_implies_Equation4552 (G: Type*) [Magma G] (h: Equation4 G) : E
   intro x y z w u
   rw [<-h, <-h, <-h]
 
+theorem Equation46_implies_Equation42 (G: Type*) [Magma G] (h: Equation46 G) : Equation42 G :=
+  fun _ _ _ => h _ _ _ _
+
+theorem Equation46_implies_Equation387 (G: Type*) [Magma G] (h: Equation46 G) : Equation387 G :=
+  fun _ _ => h _ _ _ _
+
+theorem Equation46_implies_Equation4582 (G: Type*) [Magma G] (h: Equation46 G) : Equation4582 G :=
+  fun _ _ _ _ _ _ => h _ _ _ _
+
 /-- This proof is from https://mathoverflow.net/a/450905/766 -/
 theorem Equation387_implies_Equation43 (G: Type*) [Magma G] (h: Equation387 G) : Equation43 G := by
   have idem (x : G) : (x ∘ x) ∘ (x ∘ x) = (x ∘ x) := by
@@ -108,17 +111,15 @@ theorem Equation387_implies_Equation43 (G: Type*) [Magma G] (h: Equation387 G) :
   intro x y
   rw [<- op_idem, comm, op_idem]
 
-theorem Equation4582_implies_Equation4552 (G: Type*) [Magma G] (h: Equation4582 G) : Equation4552 G :=
-  fun _ _ _ _ _ => h _ _ _ _ _ _
+theorem Equation4513_implies_Equation4512 (G: Type*) [Magma G] (h: Equation4513 G) : Equation4512 G :=
+  fun _ _ _ => h _ _ _ _
 
 theorem Equation4552_implies_Equation4513 (G: Type*) [Magma G] (h: Equation4552 G) : Equation4513 G :=
   fun _ _ _ _ => h _ _ _ _ _
 
-theorem Equation4513_implies_Equation4512 (G: Type*) [Magma G] (h: Equation4513 G) : Equation4512 G :=
-  fun _ _ _ => h _ _ _ _
+theorem Equation4582_implies_Equation4552 (G: Type*) [Magma G] (h: Equation4582 G) : Equation4552 G :=
+  fun _ _ _ _ _ => h _ _ _ _ _ _
 
-theorem Equation1_true (G: Type*) [Magma G] : Equation1 G :=
-  fun _ => rfl
 
 
 
@@ -148,26 +149,13 @@ theorem Equation3_not_implies_Equation4512 : ∃ (G: Type) (_: Magma G), Equatio
   replace h := h 1 2 3
   simp [hG] at h
 
-theorem Equation46_not_implies_Equation3 : ∃ (G: Type) (_: Magma G), Equation46 G ∧ ¬ Equation3 G := by
+theorem Equation4_not_implies_Equation43 : ∃ (G: Type) (_: Magma G), Equation4 G ∧ ¬ Equation43 G := by
   let hG : Magma Nat := {
-    op := fun _ _ => 0
+    op := fun x _ => x
   }
   use ℕ, hG
   constructor
-  . intro _ _ _ _
-    rfl
-  by_contra h
-  replace h := h 1
-  dsimp [hG] at h
-  linarith
-
-theorem Equation46_not_implies_Equation4 : ∃ (G: Type) (_: Magma G), Equation46 G ∧ ¬ Equation4 G := by
-  let hG : Magma Nat := {
-    op := fun _ _ => 0
-  }
-  use ℕ, hG
-  constructor
-  . intro _ _ _ _
+  . intro _ _
     rfl
   by_contra h
   replace h := h 1 0
@@ -184,47 +172,6 @@ theorem Equation4_not_implies_Equation4582 : ∃ (G: Type) (_: Magma G), Equatio
     rfl
   by_contra h
   replace h := h 0 0 0 1 0 0
-  dsimp [hG] at h
-  linarith
-
-theorem Equation4_not_implies_Equation43 : ∃ (G: Type) (_: Magma G), Equation4 G ∧ ¬ Equation43 G := by
-  let hG : Magma Nat := {
-    op := fun x _ => x
-  }
-  use ℕ, hG
-  constructor
-  . intro _ _
-    rfl
-  by_contra h
-  replace h := h 1 0
-  dsimp [hG] at h
-  linarith
-
-theorem Equation4582_not_implies_Equation42 : ∃ (G: Type) (_: Magma G), Equation4582 G ∧ ¬ Equation42 G := by
-  let hG : Magma Nat := {
-    op := fun x y => if x = 0 ∧ y = 0 then 1 else 2
-  }
-  use ℕ, hG
-  constructor
-  . intro x y z w u v
-    simp [hG]
-    split_ifs <;> simp_all
-  by_contra h
-  replace h := h 0 0 1
-  dsimp [hG] at h
-  linarith
-
-theorem Equation4582_not_implies_Equation43 : ∃ (G: Type) (_: Magma G), Equation4582 G ∧ ¬ Equation43 G := by
-  let hG : Magma Nat := {
-    op := fun x y => if x = 1 ∧ y = 2 then 3 else 4
-  }
-  use ℕ, hG
-  constructor
-  . intro x y z w u v
-    simp [hG]
-    split_ifs <;> simp_all
-  by_contra h
-  replace h := h 1 2
   dsimp [hG] at h
   linarith
 
@@ -303,45 +250,29 @@ theorem Equation43_not_implies_Equation4512 : ∃ (G: Type) (_: Magma G), Equati
   dsimp [hG] at h
   linarith
 
-theorem Equation4513_not_implies_Equation4552 : ∃ (G: Type) (_: Magma G), Equation4513 G ∧ ¬ Equation4552 G := by
+theorem Equation46_not_implies_Equation3 : ∃ (G: Type) (_: Magma G), Equation46 G ∧ ¬ Equation3 G := by
   let hG : Magma Nat := {
-    op := fun x y => if x = 0 then (if y ≤ 2 then 1 else 2) else x
+    op := fun _ _ => 0
   }
   use ℕ, hG
   constructor
-  . intro x y z w
-    simp [hG]
-    split_ifs <;> simp_all
+  . intro _ _ _ _
+    rfl
   by_contra h
-  replace h := h 0 0 0 3 3
+  replace h := h 1
   dsimp [hG] at h
   linarith
 
-theorem Equation4512_not_implies_Equation4513 : ∃ (G: Type) (_: Magma G), Equation4512 G ∧ ¬ Equation4513 G := by
+theorem Equation46_not_implies_Equation4 : ∃ (G: Type) (_: Magma G), Equation46 G ∧ ¬ Equation4 G := by
   let hG : Magma Nat := {
-    op := fun x y => x + y
+    op := fun _ _ => 0
   }
   use ℕ, hG
   constructor
-  . intro x y z
-    simp [hG]
-    abel
+  . intro _ _ _ _
+    rfl
   by_contra h
-  replace h := h 0 0 0 1
-  dsimp [hG] at h
-  linarith
-
-theorem Equation4512_not_implies_Equation42 : ∃ (G: Type) (_: Magma G), Equation4512 G ∧ ¬ Equation42 G := by
-  let hG : Magma Nat := {
-    op := fun x y => x + y
-  }
-  use ℕ, hG
-  constructor
-  . intro x y z
-    simp [hG]
-    abel
-  by_contra h
-  replace h := h 0 0 1
+  replace h := h 1 0
   dsimp [hG] at h
   linarith
 
@@ -369,3 +300,73 @@ theorem Equation387_not_implies_Equation4512 : ∃ (G: Type) (_: Magma G), Equat
   by_contra h
   replace h := h 0 0 1
   field_simp [hG] at h
+
+theorem Equation4512_not_implies_Equation42 : ∃ (G: Type) (_: Magma G), Equation4512 G ∧ ¬ Equation42 G := by
+  let hG : Magma Nat := {
+    op := fun x y => x + y
+  }
+  use ℕ, hG
+  constructor
+  . intro x y z
+    simp [hG]
+    abel
+  by_contra h
+  replace h := h 0 0 1
+  dsimp [hG] at h
+  linarith
+
+theorem Equation4512_not_implies_Equation4513 : ∃ (G: Type) (_: Magma G), Equation4512 G ∧ ¬ Equation4513 G := by
+  let hG : Magma Nat := {
+    op := fun x y => x + y
+  }
+  use ℕ, hG
+  constructor
+  . intro x y z
+    simp [hG]
+    abel
+  by_contra h
+  replace h := h 0 0 0 1
+  dsimp [hG] at h
+  linarith
+
+theorem Equation4513_not_implies_Equation4552 : ∃ (G: Type) (_: Magma G), Equation4513 G ∧ ¬ Equation4552 G := by
+  let hG : Magma Nat := {
+    op := fun x y => if x = 0 then (if y ≤ 2 then 1 else 2) else x
+  }
+  use ℕ, hG
+  constructor
+  . intro x y z w
+    simp [hG]
+    split_ifs <;> simp_all
+  by_contra h
+  replace h := h 0 0 0 3 3
+  dsimp [hG] at h
+  linarith
+
+theorem Equation4582_not_implies_Equation42 : ∃ (G: Type) (_: Magma G), Equation4582 G ∧ ¬ Equation42 G := by
+  let hG : Magma Nat := {
+    op := fun x y => if x = 0 ∧ y = 0 then 1 else 2
+  }
+  use ℕ, hG
+  constructor
+  . intro x y z w u v
+    simp [hG]
+    split_ifs <;> simp_all
+  by_contra h
+  replace h := h 0 0 1
+  dsimp [hG] at h
+  linarith
+
+theorem Equation4582_not_implies_Equation43 : ∃ (G: Type) (_: Magma G), Equation4582 G ∧ ¬ Equation43 G := by
+  let hG : Magma Nat := {
+    op := fun x y => if x = 1 ∧ y = 2 then 3 else 4
+  }
+  use ℕ, hG
+  constructor
+  . intro x y z w u v
+    simp [hG]
+    split_ifs <;> simp_all
+  by_contra h
+  replace h := h 1 2
+  dsimp [hG] at h
+  linarith
