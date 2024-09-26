b5efe70934a38f0e1b63079315ada3b860dcbee4
Terence Tao <tao@math.ucla.edu>
2024-09-26 13:15:25 -0700

    Add Equation 168

--- equational_theories/Basic.lean
+++ equational_theories/Basic.lean
@@ -13,21 +13,46 @@ class Magma (α : Type u) where
 
 /- List of equational laws being studied -/
 
+/-- The reflexive law -/
 def Equation1 (G: Type*) [Magma G] := ∀ x : G, x = x
+
+/-- The singleton law -/
 def Equation2 (G: Type*) [Magma G] := ∀ x y : G, x = y
+
+/-- The idempotence law -/
 def Equation3 (G: Type*) [Magma G] := ∀ x : G, x = x ∘ x
+
+/-- The left absorption law -/
 def Equation4 (G: Type*) [Magma G] := ∀ x y : G, x = x ∘ y
+
+/-- The right absorption law -/
 def Equation5 (G: Type*) [Magma G] := ∀ x y : G, x = y ∘ x
+
 def Equation6 (G: Type*) [Magma G] := ∀ x y : G, x = y ∘ y
+
 def Equation7 (G: Type*) [Magma G] := ∀ x y z : G, x = y ∘ z
+
 def Equation8 (G: Type*) [Magma G] := ∀ x : G, x = x ∘ (x ∘ x)
+
 def Equation42 (G: Type*) [Magma G] := ∀ x y z : G, x ∘ y = x ∘ z
+
 def Equation43 (G: Type*) [Magma G] := ∀ x y : G, x ∘ y = y ∘ x
+
+/-- The constant law -/
 def Equation46 (G: Type*) [Magma G] := ∀ x y z w : G, x ∘ y = z ∘ w
+
+/-- The central groupoid law -/
+def Equation168 (G: Type*) [Magma G] := ∀ x y z : G, x = (y ∘ x) ∘ (x ∘ z)
+
 def Equation387 (G: Type*) [Magma G] := ∀ x y : G, x ∘ y = (y ∘ y) ∘ x
+
+/-- The associative law -/
 def Equation4512 (G: Type*) [Magma G] := ∀ x y z : G, x ∘ (y ∘ z) = (x ∘ y) ∘ z
+
 def Equation4513 (G: Type*) [Magma G] := ∀ x y z w : G, x ∘ (y ∘ z) = (x ∘ y) ∘ w
+
 def Equation4552 (G: Type*) [Magma G] := ∀ x y z w u : G, x ∘ (y ∘ z) = (x ∘ w) ∘ u
+
 def Equation4582 (G: Type*) [Magma G] := ∀ x y z w u v: G, x ∘ (y ∘ z) = (w ∘ u) ∘ v
 
 
