a68f218bb02d040dcd0167ef12204dba57e6c225
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2024-09-28 16:35:45 +0200

    golf `Subgraph.lean`

--- equational_theories/Subgraph.lean
+++ equational_theories/Subgraph.lean
@@ -111,11 +111,8 @@ theorem Equation7_implies_Equation2 (G: Type*) [Magma G] (h: Equation7 G) : Equa
 theorem Equation7_implies_Equation41 (G: Type*) [Magma G] (h: Equation7 G) : Equation41 G :=
   fun _ _ _ ↦ by rw [← h]
 
-theorem Equation38_implies_Equation42 (G: Type*) [Magma G] (h: Equation38 G) : Equation42 G := by
-  intro x y z
-  calc
-    x ∘ y = x ∘ x := by rw [h]
-    _ = x ∘ z := by rw [h]
+theorem Equation38_implies_Equation42 (G: Type*) [Magma G] (h: Equation38 G) : Equation42 G :=
+  fun _ _ _ ↦ by rw [← h, h]
 
 theorem Equation41_implies_Equation40 (G: Type*) [Magma G] (h: Equation41 G) : Equation40 G :=
   fun _ _ ↦ by rw [h]
@@ -123,9 +120,8 @@ theorem Equation41_implies_Equation40 (G: Type*) [Magma G] (h: Equation41 G) : E
 theorem Equation41_implies_Equation46 (G: Type*) [Magma G] (h: Equation41 G) : Equation46 G :=
   fun _ _ _ _ ↦ by rwa [← h, h]
 
-theorem Equation42_implies_Equation38 (G: Type*) [Magma G] (h: Equation42 G) : Equation38 G := by
-  intro x y
-  rw [h x x y]
+theorem Equation42_implies_Equation38 (G: Type*) [Magma G] (h: Equation42 G) : Equation38 G :=
+  fun _ _ ↦ by rw [h]
 
 theorem Equation46_implies_Equation40 (G: Type*) [Magma G] (h: Equation46 G) : Equation40 G :=
   fun x y ↦ h x x y y
