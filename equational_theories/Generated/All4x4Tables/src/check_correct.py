4ab170b666b119cfbb59df4d14a11a4f6e78cde2
Nicholas Carlini <nicholas@carlini.com>
2024-09-29 14:50:59 -0700

    Script to check that all generated refutations are correct

---
+++ equational_theories/Generated/All4x4Tables/src/check_correct.py
@@ -0,0 +1,166 @@
+import numpy as np
+import collections
+import ast
+from itertools import product
+
+from itertools import product
+import re
+from typing import List, Callable
+
+import re
+
+# Define the expression node
+class ExprNode:
+    def __init__(self, value, left=None, right=None):
+        self.value = value  # Operator or operand
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        if self.left and self.right:
+            return f"({self.left} {self.value} {self.right})"
+        return self.value
+
+# Parser implementation
+class Parser:
+    def __init__(self, expression):
+        self.expression = expression.replace(' ', '')
+        self.index = 0
+        self.length = len(self.expression)
+
+    def parse(self):
+        return self.parse_expression()
+
+    def parse_expression(self):
+        nodes = [self.parse_term()]
+
+        while self.current_char() == '∘':
+            op = self.current_char()
+            self.advance()
+            right = self.parse_term()
+            nodes.append(op)
+            nodes.append(right)
+
+        # Build the tree (left-associative)
+        node = nodes[0]
+        for i in range(1, len(nodes), 2):
+            node = ExprNode(nodes[i], left=node, right=nodes[i+1])
+
+        return node
+
+    def parse_term(self):
+        char = self.current_char()
+        if char == '(':
+            self.advance()
+            node = self.parse_expression()
+            if self.current_char() != ')':
+                raise ValueError("Mismatched parentheses")
+            self.advance()
+            return node
+        else:
+            return self.parse_variable()
+
+    def parse_variable(self):
+        match = re.match(r'[a-zA-Z_]\w*', self.expression[self.index:])
+        if not match:
+            raise ValueError(f"Invalid character at index {self.index}")
+        var = match.group(0)
+        self.index += len(var)
+        return ExprNode(var)
+
+    def current_char(self):
+        if self.index < self.length:
+            return self.expression[self.index]
+        return None
+
+    def advance(self):
+        self.index += 1
+
+# Function to convert expression tree to prefix notation
+def expr_to_prefix(node):
+    if node.value == '∘':
+        left = expr_to_prefix(node.left)
+        right = expr_to_prefix(node.right)
+        return f"f({left}, {right})"
+    else:
+        return node.value
+
+# Function to convert equations to lambdas
+ctr = 0
+def convert(vars_list, equation):
+    global ctr
+    if True:
+        lhs_expr, rhs_expr = equation.split('=')
+
+        # Parse LHS
+        parser_lhs = Parser(lhs_expr)
+        tree_lhs = parser_lhs.parse()
+        prefix_lhs = expr_to_prefix(tree_lhs)
+
+        # Parse RHS
+        parser_rhs = Parser(rhs_expr)
+        tree_rhs = parser_rhs.parse()
+        prefix_rhs = expr_to_prefix(tree_rhs)
+
+        # Create lambda string
+        lambda_str = f"lambda f,{', '.join(vars_list)}: {prefix_lhs} == {prefix_rhs}"
+
+        # Compile the lambda function
+        try:
+            lambda_func = eval(lambda_str)
+        except Exception as e:
+            print(f"Error compiling lambda{idx}: {e}")
+            lambda_func = None
+
+        # Store with a name
+        lambda_name = f"lambda"
+        #print(ctr, lambda_str)
+        ctr += 1
+
+        return lambda_func
+
+def get_fns():
+    equations = open("equations.txt", "r").read().split("\n")[:-1]
+    fns = []
+    for eq in equations:
+        oeq = eq
+        eq = eq.split("∀")[1]
+        variables, eq = eq.split(":")
+        variables = variables.strip().split()
+        rule = eq.split(",")[1]
+        fns.append((oeq, len(variables), convert(variables, rule)))
+    
+    return fns
+
+fns = get_fns()
+
+def check_rule(nvar, check, S, op):
+    for args in product(S, repeat=nvar):
+        if not check(op, *args):
+            return False
+    return True
+
+S = list(range(4))
+
+
+for row in open("../data/refutations.txt").readlines()[3*30:]:
+    if 'Table' in row:
+        print()
+        print()
+        print(row.strip())
+        table = np.array(eval(row.split("Table")[1]))[::-1]
+    elif 'Proves' in row:
+        proves = eval(row.split("Proves")[1])
+        print(proves)
+
+        ok = []
+        for x in proves:
+            string, nvar, fn = fns[x-1]
+            op = lambda a,b: table[a][b]
+
+            this = check_rule(nvar, fn, S, op)
+            #print(this, string)
+            
+            ok.append(this)
+        print(collections.Counter(ok))
+
