022d8de73ed0cd2f90894c7d8ec27900072d0e61
Shrys <shreyasss94@gmail.com>
2024-09-29 22:13:39 +0000

    Tagged magma law notation

--- equational_theories/MagmaLaw.lean
+++ equational_theories/MagmaLaw.lean
@@ -6,7 +6,7 @@ structure MagmaLaw.{u} (α : Type u) where
   lhs : FreeMagma α
   rhs : FreeMagma α
 
-notation:50(priority:=999) "Magma Law: " l " ≃ " r => MagmaLaw.mk l r
+notation:50(priority:=999) "magma law: " l " ≃ " r => MagmaLaw.mk l r
 
 universe u
 variable {α : Type u} {x y z : FreeMagma α}
@@ -14,8 +14,11 @@ variable {α : Type u} {x y z : FreeMagma α}
 -- examples to test
 def combine (x y : FreeMagma α) := x ⋆ (y ⋆ y)
 
-#check Magma Law: x ⋆ (combine x y) ⋆ z ≃ z
-#reduce Magma Law: x ⋆ (combine x z) ≃ y -- result : Magma Law: x ⋆ (x ⋆ (z ⋆ z)) ≃ y
+#check magma law: x ⋆ (combine x y) ⋆ z ≃ z
+-- Infoview Output: magma law: x ⋆ combine x y ⋆ z ≃ z : MagmaLaw α
+
+#reduce magma law: x ⋆ (combine x z) ≃ y
+-- Infoview Output: magma law: x ⋆ (x ⋆ (z ⋆ z)) ≃ y
 
 
 end MagmaLaw
