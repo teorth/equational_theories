2d7812df4462e902374e630e45ab308fa61f8df1
Shrys <shreyasss94@gmail.com>
2024-09-29 21:56:02 +0000

    Extra examples of Magma Law

--- equational_theories/MagmaLaw.lean
+++ equational_theories/MagmaLaw.lean
@@ -10,6 +10,11 @@ notation:50(priority:=999) l " ≃ " r => MagmaLaw.mk l r
 
 universe u
 variable {α : Type u} {x y z : FreeMagma α}
-#check x ⋆ y ⋆ z ≃ z
+
+-- examples to test
+def w (x y : FreeMagma α) := x ⋆ y
+#check x ⋆ (w x y) ⋆ z ≃ z
+#reduce x ⋆ (w x z)
+
 
 end MagmaLaw
