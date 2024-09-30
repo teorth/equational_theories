29b686d0d2571fc1145e18189c21e915fdd2fcd6
madvorak <martin.dvorak@matfyz.cz>
2024-09-30 10:50:46 +0200

    utilize dot notations

--- equational_theories/Subgraph.lean
+++ equational_theories/Subgraph.lean
@@ -173,7 +173,7 @@ conjecture Equation14_implies_Equation29 (G: Type*) [Magma G] (h: Equation14 G) 
 /-- This implication is Problem A1 from Putnam 2001 -/
 @[equational_result]
 theorem Equation29_implies_Equation14 (G: Type*) [Magma G] (h: Equation29 G) : Equation14 G :=
-  fun x y ↦ Eq.trans (h x (x ∘ y)) (congrArg (fun z ↦ z ∘ (x ∘ y)) (Eq.symm (h y x)))
+  fun x y ↦ (h x (x ∘ y)).trans (congrArg (· ∘ (x ∘ y)) (h y x).symm)
 
 @[equational_result]
 theorem Equation38_implies_Equation42 (G: Type*) [Magma G] (h: Equation38 G) : Equation42 G :=
@@ -238,10 +238,10 @@ theorem Equation387_implies_Equation43 (G: Type*) [Magma G] (h: Equation387 G) :
 /-- Putnam 1978, problem A4, part (b) -/
 @[equational_result]
 theorem Equation3744_implies_Equation381 (G : Type*) [Magma G] (h: Equation3744 G) : Equation381 G :=
-  fun x y z ↦ Eq.trans
-    (h x y z y) $ Eq.trans
-    (congrArg (fun a ↦ a ∘ (y ∘ y)) (h x z z x))
-    (Eq.symm $ h (x ∘ z) y (x ∘ z) y)
+  fun x y z ↦
+    (h x y z y).trans $
+    (congrArg (· ∘ (y ∘ y)) (h x z z x)).trans
+    (h (x ∘ z) y (x ∘ z) y).symm
 
 /-- Putnam 1978, problem A4, part (a) -/
 @[equational_result]
