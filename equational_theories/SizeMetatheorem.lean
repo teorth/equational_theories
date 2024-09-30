ae0b1b9ced11578a739bfbdc1d98121e960dde9b
Cody Roux <codyroux@amazon.com>
2024-09-30 19:48:32 +0000

    Rename some lemmas and defs to be consistent.

--- equational_theories/SizeMetatheorem.lean
+++ equational_theories/SizeMetatheorem.lean
@@ -12,25 +12,25 @@ by
 
 -- We use min here, as we want terms of *at least* size n.
 @[simp]
-def MagmaLaw.size {α} (E : MagmaLaw α) : Nat := min (FreeMagma.order E.lhs) (FreeMagma.order E.rhs)
+def MagmaLaw.order {α} (E : MagmaLaw α) : Nat := min (FreeMagma.order E.lhs) (FreeMagma.order E.rhs)
 
 @[inline, simp]
-def Ctx.IsOfSize {α} (Γ : Ctx α) (n : Nat) := ∀ E ∈ Γ, n ≤ MagmaLaw.size E
+def Ctx.IsOfOrder {α} (Γ : Ctx α) (n : Nat) := ∀ E ∈ Γ, n ≤ MagmaLaw.order E
 
-theorem derive.PreservesSize {α} (Γ : Ctx α) (n : Nat)(sizeBound : Ctx.IsOfSize Γ n) (E : MagmaLaw α)(h : Γ ⊢ E) :
-  E.lhs = E.rhs ∨ n ≤ MagmaLaw.size E :=
+theorem derive.PreservesOrder {α} (Γ : Ctx α) (n : Nat)(sizeBound : Ctx.IsOfOrder Γ n) (E : MagmaLaw α)(h : Γ ⊢ E) :
+  E.lhs = E.rhs ∨ n ≤ MagmaLaw.order E :=
 by
   cases h
   case Ax => right; apply sizeBound; trivial
   case Ref => left; simp
   case Sym _ _ h =>
-    cases (derive.PreservesSize _ _ sizeBound _ h)
+    cases (derive.PreservesOrder _ _ sizeBound _ h)
     case inl eq =>
       left; simp at *; rw [eq]
     case inr h =>
       right; simp at *; omega
   case Trans _ _ _ h₁ h₂ =>
-    cases (derive.PreservesSize _ _ sizeBound _ h₁) <;> cases (derive.PreservesSize _ _ sizeBound _ h₂) <;> simp at *
+    cases (derive.PreservesOrder _ _ sizeBound _ h₁) <;> cases (derive.PreservesOrder _ _ sizeBound _ h₂) <;> simp at *
     case inl.inl h₁ h₂ =>
       left;
       rw [h₁, h₂]
@@ -41,7 +41,7 @@ by
     case inr.inr =>
       right; omega
   case Subst t u σ h =>
-    cases (derive.PreservesSize _ _ sizeBound _ h) <;> simp at *
+    cases (derive.PreservesOrder _ _ sizeBound _ h) <;> simp at *
     case inl h => left; rw [h]
     case inr =>
       right
@@ -49,7 +49,7 @@ by
       have h₂ := FreeMagma.orderLtSubst u σ
       omega
   case Cong h₁ h₂ =>
-    cases (derive.PreservesSize _ _ sizeBound _ h₁) <;> cases (derive.PreservesSize _ _ sizeBound _ h₂) <;> simp at *
+    cases (derive.PreservesOrder _ _ sizeBound _ h₁) <;> cases (derive.PreservesOrder _ _ sizeBound _ h₂) <;> simp at *
     case inl.inl h₁ h₂ =>
       left;
       rw [h₁, h₂]; constructor <;> trivial
@@ -61,8 +61,8 @@ by
     case inr.inr =>
       right; omega
 
-theorem models.PreservesSize {α} (Γ : Ctx α) (n : Nat)(sizeBound : Ctx.IsOfSize Γ n) (E : MagmaLaw α)(h : Γ ⊧ E) :
-  E.lhs = E.rhs ∨ n ≤ MagmaLaw.size E :=
+theorem models.PreservesOrder {α} (Γ : Ctx α) (n : Nat)(sizeBound : Ctx.IsOfOrder Γ n) (E : MagmaLaw α)(h : Γ ⊧ E) :
+  E.lhs = E.rhs ∨ n ≤ MagmaLaw.order E :=
 by
   have ⟨ h ⟩ := Completeness _ _ h
-  apply derive.PreservesSize <;> trivial
+  apply derive.PreservesOrder <;> trivial
