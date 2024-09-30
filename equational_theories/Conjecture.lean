fe068a2ae73ff5b8f8e2b205792a99a757e483fd
Joachim Breitner <mail@joachim-breitner.de>
2024-09-30 14:14:42 +0200

    Let @[equational_result] handle facts (#130)
    * Let equational_result handle facts
    
    * Update equational_theories/ParseImplications.lean
    
    * Update equational_theories/ParseImplications.lean

--- equational_theories/Conjecture.lean
+++ equational_theories/Conjecture.lean
@@ -19,7 +19,7 @@ def «conjecture» := leading_parser
 -/
 inductive EntryVariant where
   | implication : Implication → EntryVariant
-  | nonimplication : Implication → EntryVariant
+  | facts : Facts → EntryVariant
   | other : EntryVariant
 
 /-- An entry in the conjecture environment extension -/
@@ -57,8 +57,8 @@ def elabConjecture : CommandElab
         liftCoreM <| Meta.MetaM.run' do
         if let some imp ← parseImplication val.type then
           return some ⟨val.name, .implication imp⟩
-        if let some nimp ← parseNonimplication val.type then
-          return some ⟨val.name, .nonimplication nimp⟩
+        if let some facts ← parseFacts val.type then
+          return some ⟨val.name, .facts facts⟩
         return some ⟨val.name, .other⟩
       | _ => pure none
 
@@ -84,5 +84,5 @@ elab_rules : command
   for ⟨name, conj⟩ in cs do
     match conj with
     | .implication ⟨lhs, rhs⟩ => println! "{name}: {lhs} → {rhs}"
-    | .nonimplication ⟨lhs, rhs⟩ => println! "{name}: ¬ ({lhs} → {rhs})"
+    | .facts ⟨satisfied, refuted⟩ => println! "{name}: {satisfied} // {refuted}"
     | _ => println! "{name}"
