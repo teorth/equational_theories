e0fe315697d9e608b818d4436c4f4782e62639a2
Daniel Weber <commandmaster2017@gmail.com>
2024-09-30 17:46:51 +0300

    Update blueprint

--- equational_theories/Counting.lean
+++ equational_theories/Counting.lean
@@ -1,3 +1,7 @@
+/-
+Copyright (c) 2022 Julian Kuelshammer. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+-/
 import equational_theories.FreeMagma
 import Mathlib.Combinatorics.Enumerative.Catalan
 
