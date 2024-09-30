8cc1cd86c7244668778c868c87580b309aea4170
Joachim Breitner <mail@joachim-breitner.de>
2024-09-30 14:32:47 +0200

    Massive generation of refutations via polynomials over Z/nZ (#19)
    * Massive generation of refutations via polynomials over cyclic groups
    
    * Use compact syntax to good effect
    
    * Use compact syntax, actually commit files
    
    * Add counterexamples as separate lib
    
    * Do not generate empty files
    
    * Remove debugging check
    
    * Make DecideBang tactic compiled
    
    * Fix bad formula mangling
    
    * Introduce decideFin! tactic to avoid type inference overhead. 2.5× times faster!
    
    * linter.unusedVariables false
    
    * Build counter-examples not parallel
    
    * Remove example, now that we can commit the files
    
    * Docstring and other cosmetics
    
    * Import more refutations
    
    * Only include magmas with <5 elements for now
    
    * Try to use native decide
    
    * Try to balance the large conjunction
    
    * Hidingt the inst in a top-level declaration does not help
    
    * Revert
    
    * Use Magma file, mild refactoring
    
    * Memoize monoid op
    
    * Typos
    
    * Put AllEquations into a namespace for now
    
    * Move DecideBang back into main library
    
    * SmallMagmas file
    
    * Different examples
    
    * Move to Generated
    
    * Prune counter-examples based on implications
    
    * Prune much more
    
    * Include more magmas
    
    * Remove dead code
    
    * Also prune based on covering
    
    * Set equational_result

---
+++ equational_theories/Generated/FinitePoly/Refutation84.lean
@@ -0,0 +1,23 @@
+
+import equational_theories.AllEquations
+import equational_theories.FactsSyntax
+import equational_theories.MemoFinOp
+import equational_theories.DecideBang
+
+/-!
+This file is generated from the following refutation as produced by
+random generation of polynomials:
+'(0 * x**2 + 2 * y**2 + 0 * x + 0 * y + 1 * x * y) % 3' (0, 39, 306, 309, 311, 315, 319, 3252, 3255, 3257, 3261, 3265, 3267, 3271, 3275, 3277, 3281, 3285, 3287, 3292, 3297, 3302, 3658, 3661, 3663, 3664, 3667, 3671, 3673, 3676, 3677, 3681, 3683, 3687, 3691, 3693, 3698, 3699, 3703, 3708, 4269, 4271, 4275, 4279, 4340, 4342, 4345, 4350, 4354, 4589, 4621)
+-/
+
+set_option linter.unusedVariables false
+
+/-! The magma definition -/
+def «FinitePoly 2 * y² + x * y % 3» : Magma (Fin 3) where
+  op := memoFinOp fun x y => 2 * y*y + x * y
+
+/-! The facts -/
+@[equational_result]
+theorem «Facts from FinitePoly 2 * y² + x * y % 3» :
+  ∃ (G : Type) (_ : Magma G), Facts G [320, 3303, 3662, 3668, 3678, 3700, 4346, 4622] [323, 3306] :=
+    ⟨Fin 3, «FinitePoly 2 * y² + x * y % 3», by decideFin!⟩
