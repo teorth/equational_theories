bf40c190e67970af3fe7b0bce00bc511f6e17f70
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2024-10-01 00:26:11 +0200

    golf Compactness

--- equational_theories/Compactness.lean
+++ equational_theories/Compactness.lean
@@ -1,21 +1,21 @@
 import equational_theories.Completeness
 import Mathlib.Data.Finset.Basic
 
-def derive.getAxioms {α} [DecidableEq α] {Γ : Ctx α} {E : MagmaLaw α}(h : Γ ⊢ E) : Finset (MagmaLaw α) :=
-match h with
-| .Ax _ E _ => {E}
-| .Ref _ _ => {}
-| .Sym _ _ _ h => derive.getAxioms h
-| .Trans _ _ _ _ h₁ h₂ => derive.getAxioms h₁ ∪ derive.getAxioms h₂
-| .Subst _ _ _ _ h => derive.getAxioms h
-| .Cong _ _ _ _ _ h₁ h₂ => derive.getAxioms h₁ ∪ derive.getAxioms h₂
+def derive.getAxioms {α} [DecidableEq α] {Γ : Ctx α} {E : MagmaLaw α} (h : Γ ⊢ E) :
+    Finset (MagmaLaw α) :=
+  match h with
+  | .Ax _ E _ => {E}
+  | .Ref _ _ => {}
+  | .Sym _ _ _ h => derive.getAxioms h
+  | .Trans _ _ _ _ h₁ h₂ => derive.getAxioms h₁ ∪ derive.getAxioms h₂
+  | .Subst _ _ _ _ h => derive.getAxioms h
+  | .Cong _ _ _ _ _ h₁ h₂ => derive.getAxioms h₁ ∪ derive.getAxioms h₂
 
 def ToCtx {α} (S : Set (MagmaLaw α)) : Ctx α := S
 
 instance Ctx.hasSubset {α} : HasSubset (Ctx α) := Set.instHasSubset
 
-theorem foo (S T : Set Nat) : S ⊆ S ∪ T :=
-by exact Set.subset_union_left
+theorem foo (S T : Set Nat) : S ⊆ S ∪ T := Set.subset_union_left
 
 def derive.Weak {α} {Γ Δ : Ctx α}{E : MagmaLaw α}(inc : Γ ⊆ Δ) (h : Γ ⊢ E) :
   Δ ⊢ E :=
@@ -32,43 +32,22 @@ def derive.getAxiomsEnough {α} [DecidableEq α] {Γ : Ctx α} {E : MagmaLaw α}
   ToCtx (derive.getAxioms h) ⊢ E :=
 by
   cases h <;> simp [ToCtx, getAxioms]
-  case Ax =>
-    constructor; simp
-  case Ref =>
-    apply derive.Ref
-  case Sym _ _ h =>
-    apply derive.Sym
-    apply derive.getAxiomsEnough
+  case Ax => constructor; trivial
+  case Ref => exact derive.Ref _ _
+  case Sym _ _ h => exact derive.Sym _ _ _ (derive.getAxiomsEnough _)
   case Trans _ _ _ h₁ h₂ =>
     apply derive.Trans
-    . have j := derive.getAxiomsEnough h₁
-      apply derive.Weak
-      . apply Set.subset_union_left
-      . exact j
-    . have j := derive.getAxiomsEnough h₂
-      apply derive.Weak
-      . apply Set.subset_union_right
-      . exact j
-  case Subst =>
-    apply derive.Subst
-    apply derive.getAxiomsEnough
+    · exact derive.Weak Set.subset_union_left (derive.getAxiomsEnough h₁)
+    · exact derive.Weak Set.subset_union_right (derive.getAxiomsEnough h₂)
+  case Subst => exact derive.Subst _ _ _ _ (derive.getAxiomsEnough _)
   case Cong _ _ _ h₁ h₂ =>
     apply derive.Cong
-    . have j := derive.getAxiomsEnough h₁
-      apply derive.Weak
-      . apply Set.subset_union_left
-      . exact j
-    . have j := derive.getAxiomsEnough h₂
-      apply derive.Weak
-      . apply Set.subset_union_right
-      . exact j
+    · exact derive.Weak Set.subset_union_left (derive.getAxiomsEnough h₁)
+    · exact derive.Weak Set.subset_union_right (derive.getAxiomsEnough h₂)
 
 def Compactness {α} [DecidableEq α] {Γ : Ctx α} {E : MagmaLaw α}(h : Γ ⊧ E) :
-  ∃ (Δ : Finset (MagmaLaw α)), Nonempty <| ToCtx Δ ⊧ E :=
-by
-  have h' := Completeness _ _ h
-  have ⟨ h'' ⟩ := h'
-  exists (derive.getAxioms h'')
+    ∃ (Δ : Finset (MagmaLaw α)), Nonempty <| ToCtx Δ ⊧ E := by
+  have ⟨ h'' ⟩ := Completeness _ _ h
+  refine ⟨(derive.getAxioms (h'')), ?_⟩
   constructor
-  apply Soundness
-  apply derive.getAxiomsEnough
+  apply Soundness _ _ (derive.getAxiomsEnough _)
