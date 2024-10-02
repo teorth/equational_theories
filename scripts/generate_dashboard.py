6e6e9a856bff1e8cfc62c8e7424cac0067db6726
David Renshaw <dwrenshaw@gmail.com>
2024-10-02 14:43:58 -0400

    move dashboard.md -> dashboard/index.md. add links in readme and topbar

--- scripts/generate_dashboard.py
+++ scripts/generate_dashboard.py
@@ -1,12 +1,13 @@
 import argparse
 import json
+import os
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="generate the dashboard markdown")
     parser.add_argument("hist_file",
                         help="json file containing output of `lake exe extract_implications outcomes --hist")
     parser.add_argument("--out_file",
-                        default="home_page/dashboard.md",
+                        default="home_page/dashboard/index.md",
                         help="markdown file to pass to jekyll")
 
     args = parser.parse_args()
@@ -20,6 +21,10 @@ if __name__ == '__main__':
     unknown = data['unknown'];
     known_total = explicit_proof_true + implicit_proof_true + explicit_proof_false + implicit_proof_false
     total = known_total + unknown
+
+    directory = os.path.dirname(args.out_file)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
     outfile = open(args.out_file, 'w')
     outfile.write("\n")
