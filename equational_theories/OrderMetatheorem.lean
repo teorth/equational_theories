5657a204b222c5e837951c98ce6dc0bdccdd43a5
codyroux <cody.roux@gmail.com>
2024-10-02 22:59:34 -0400

    Codyroux/derivation preserves size (#152)
    * [DO NOT MERGE] Define a proof theory for the equational logic of magmas.
    
    For now just soundness.
    
    * Start adding theorems to the blueprint, clean up notation in Lean.
    
    * Fix some rebase snafus, improve notation.
    
    * Add a brief overview of the completness theorem.
    
    * Nix bluprint \lean pragmas for now, to fix build.
    
    * [FEAT] Prove compactness theorem for formal magmas.
    
    * Remove blueprint pragmas, for now.
    
    * Minor cleanup.
    
    * Minor cleanup
    
    * Prove simple meta-theorem about size of derived equalities.
    
    * Fix incorrect size definition: all sizes were 0!
    
    * Update the size metatheorem to use the existing size definition in Counting.lean.
    
    * Rename some lemmas and defs to be consistent.
    
    * Rename file as well.
    
    * Rename the file as well.
    
    * golf Subgraph
    
    * Update Subgraph.lean
    
    * Update Subgraph.lean
    
    * Publish OrderMetatheorem to the library
    
    ---------
    
    Co-authored-by: Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>

---
+++ equational_theories/OrderMetatheorem.lean
@@ -0,0 +1,68 @@
+import equational_theories.Completeness
+import equational_theories.Counting
+
+theorem FreeMagma.orderLtSubst {α} (t : FreeMagma α) (σ : α → FreeMagma α) :
+  FreeMagma.order t ≤ FreeMagma.order (t ⬝ σ) :=
+by
+  cases t <;> simp [order]
+  case Fork t u =>
+    have h₁ := FreeMagma.orderLtSubst t σ
+    have h₂ := FreeMagma.orderLtSubst u σ
+    omega
+
+-- We use min here, as we want terms of *at least* size n.
+@[simp]
+def MagmaLaw.order {α} (E : MagmaLaw α) : Nat := min (FreeMagma.order E.lhs) (FreeMagma.order E.rhs)
+
+@[inline, simp]
+def Ctx.IsOfOrder {α} (Γ : Ctx α) (n : Nat) := ∀ E ∈ Γ, n ≤ MagmaLaw.order E
+
+theorem derive.PreservesOrder {α} (Γ : Ctx α) (n : Nat)(sizeBound : Ctx.IsOfOrder Γ n) (E : MagmaLaw α)(h : Γ ⊢ E) :
+  E.lhs = E.rhs ∨ n ≤ MagmaLaw.order E :=
+by
+  cases h
+  case Ax => right; apply sizeBound; trivial
+  case Ref => left; simp
+  case Sym _ _ h =>
+    cases (derive.PreservesOrder _ _ sizeBound _ h)
+    case inl eq =>
+      left; simp at *; rw [eq]
+    case inr h =>
+      right; simp at *; omega
+  case Trans _ _ _ h₁ h₂ =>
+    cases (derive.PreservesOrder _ _ sizeBound _ h₁) <;> cases (derive.PreservesOrder _ _ sizeBound _ h₂) <;> simp at *
+    case inl.inl h₁ h₂ =>
+      left;
+      rw [h₁, h₂]
+    case inl.inr h₁ h₂ =>
+      right; rw [h₁]; omega
+    case inr.inl h₁ h₂ =>
+      right; rw [← h₁]; omega
+    case inr.inr =>
+      right; omega
+  case Subst t u σ h =>
+    cases (derive.PreservesOrder _ _ sizeBound _ h) <;> simp at *
+    case inl h => left; rw [h]
+    case inr =>
+      right
+      have h₁ := FreeMagma.orderLtSubst t σ
+      have h₂ := FreeMagma.orderLtSubst u σ
+      omega
+  case Cong h₁ h₂ =>
+    cases (derive.PreservesOrder _ _ sizeBound _ h₁) <;> cases (derive.PreservesOrder _ _ sizeBound _ h₂) <;> simp at *
+    case inl.inl h₁ h₂ =>
+      left;
+      rw [h₁, h₂]; constructor <;> trivial
+    case inl.inr h₁ h₂ =>
+      right; rw [h₁]
+      omega
+    case inr.inl h₁ h₂ =>
+      right; rw [← h₁]; omega
+    case inr.inr =>
+      right; omega
+
+theorem models.PreservesOrder {α} (Γ : Ctx α) (n : Nat)(sizeBound : Ctx.IsOfOrder Γ n) (E : MagmaLaw α)(h : Γ ⊧ E) :
+  E.lhs = E.rhs ∨ n ≤ MagmaLaw.order E :=
+by
+  have ⟨ h ⟩ := Completeness _ _ h
+  apply derive.PreservesOrder <;> trivial
