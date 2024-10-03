95fde3bff396d27884659bf843d6c3ec68f9f3ec
codyroux <cody.roux@gmail.com>
2024-10-03 15:04:24 -0400

    Update equational_theories/OrderMetatheorem.lean
    Proof golf suggested by @fpvandoorn
    
    Co-authored-by: Floris van Doorn <fpvdoorn@gmail.com>

--- equational_theories/OrderMetatheorem.lean
+++ equational_theories/OrderMetatheorem.lean
@@ -18,49 +18,31 @@ def MagmaLaw.order {α} (E : MagmaLaw α) : Nat := min (FreeMagma.order E.lhs) (
 @[inline, simp]
 def Ctx.IsOfOrder {α} (Γ : Ctx α) (n : Nat) := ∀ E ∈ Γ, n ≤ MagmaLaw.order E
 
-theorem derive.PreservesOrder {α} (Γ : Ctx α) (n : Nat)(sizeBound : Ctx.IsOfOrder Γ n) (E : MagmaLaw α)(h : Γ ⊢ E) :
-  E.lhs = E.rhs ∨ n ≤ MagmaLaw.order E :=
-by
-  cases h
-  case Ax => right; apply sizeBound; trivial
-  case Ref => left; simp
-  case Sym _ _ h =>
-    cases (derive.PreservesOrder _ _ sizeBound _ h)
-    case inl eq =>
-      left; simp at *; rw [eq]
-    case inr h =>
-      right; simp at *; omega
-  case Trans _ _ _ h₁ h₂ =>
-    cases (derive.PreservesOrder _ _ sizeBound _ h₁) <;> cases (derive.PreservesOrder _ _ sizeBound _ h₂) <;> simp at *
-    case inl.inl h₁ h₂ =>
-      left;
-      rw [h₁, h₂]
-    case inl.inr h₁ h₂ =>
-      right; rw [h₁]; omega
-    case inr.inl h₁ h₂ =>
-      right; rw [← h₁]; omega
-    case inr.inr =>
-      right; omega
-  case Subst t u σ h =>
-    cases (derive.PreservesOrder _ _ sizeBound _ h) <;> simp at *
-    case inl h => left; rw [h]
-    case inr =>
-      right
-      have h₁ := FreeMagma.orderLtSubst t σ
-      have h₂ := FreeMagma.orderLtSubst u σ
-      omega
-  case Cong h₁ h₂ =>
-    cases (derive.PreservesOrder _ _ sizeBound _ h₁) <;> cases (derive.PreservesOrder _ _ sizeBound _ h₂) <;> simp at *
-    case inl.inl h₁ h₂ =>
-      left;
-      rw [h₁, h₂]; constructor <;> trivial
-    case inl.inr h₁ h₂ =>
-      right; rw [h₁]
-      omega
-    case inr.inl h₁ h₂ =>
-      right; rw [← h₁]; omega
-    case inr.inr =>
-      right; omega
+theorem derive.PreservesOrder {α} (Γ : Ctx α) (n : Nat) (sizeBound : Ctx.IsOfOrder Γ n)
+    (E : MagmaLaw α) (h : Γ ⊢ E) :
+    E.lhs = E.rhs ∨ n ≤ MagmaLaw.order E := by
+  rw [or_iff_not_imp_left, ← ne_eq]
+  intro h2
+  induction h with
+  | Ax E h => exact sizeBound E h
+  | Ref t => simp at h2
+  | Sym t u _ ih => simpa [MagmaLaw.order, min_comm] using ih h2.symm
+  | Trans t u v _ _ ih₁ ih₂ =>
+    simp at ih₁ ih₂ h2 ⊢
+    by_cases h : t = u
+    · subst h
+      exact ih₂ h2
+    by_cases h3 : u = v
+    · subst h3
+      exact ih₁ h2
+    exact ⟨ih₁ h |>.1, ih₂ h3 |>.2⟩
+  | Subst t u σ _ ih =>
+    simp at ih h2 ⊢
+    specialize ih (by rintro rfl; contradiction)
+    exact ⟨ih.1.trans <| t.orderLtSubst σ, ih.2.trans <| u.orderLtSubst σ⟩
+  | Cong t₁ t₂ u₁ u₂ _ _ ih₁ ih₂ => 
+    simp at ih₁ ih₂ h2 ⊢
+    by_cases h : t₁ = t₂ <;> [specialize ih₂ (h2 h); specialize ih₁ h] <;> omega
 
 theorem models.PreservesOrder {α} (Γ : Ctx α) (n : Nat)(sizeBound : Ctx.IsOfOrder Γ n) (E : MagmaLaw α)(h : Γ ⊧ E) :
   E.lhs = E.rhs ∨ n ≤ MagmaLaw.order E :=
