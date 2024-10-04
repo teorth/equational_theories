d90c713f879185638a971819837f4d9db1d7b5ea
Andrés Goens <andres@goens.org>
2024-10-04 13:11:49 +0200

    reverse pre-order direction, add Preorder.lean to equational_theories.lean for CI

--- equational_theories.lean
+++ equational_theories.lean
@@ -9,5 +9,6 @@ import equational_theories.InfModel
 import equational_theories.Generated
 import equational_theories.Counting
 import equational_theories.OrderMetatheorem
+import equational_theories.Preorder
 import equational_theories.SmallMagmas
 import equational_theories.Homomorphisms
