54371db94079420539e017566b7d84fc8bcf3416
0art0 <18333981+0art0@users.noreply.github.com>
2024-10-04 18:23:29 +0100

    Example of lists as lifting magma class

--- equational_theories/LiftingMagmaFamilies.lean
+++ equational_theories/LiftingMagmaFamilies.lean
@@ -1,17 +1,24 @@
 import equational_theories.MagmaLaw
 import equational_theories.Homomorphisms
+import equational_theories.ForMathlib.Enum.Enum
 
 open Law
 
 class LiftingMagma (G : Type → Type) where
   instMagma {α} : Magma (G α)
+  instMagmaDecidableEq {α} [DecidableEq α] : DecidableEq (G α)
   ι : ∀ {α}, α → G α
   lift : ∀ {α}, (α → G α) → (G α →◇ G α)
   lift_factors : ∀ {α}, ∀ f : α → G α, f = (lift f) ∘ ι
 
-instance {G : Type → Type} [LiftingMagma G] {α : Type} : Magma (G α) := LiftingMagma.instMagma
+variable {α : Type} {G : Type → Type} [LiftingMagma G]
 
-theorem MagmaLaw.models_iff_satisfies_ι {α : Type} {G : Type → Type} (law : MagmaLaw α) [LiftingMagma G] :
+instance : Magma (G α) := LiftingMagma.instMagma
+
+instance [DecidableEq α] : DecidableEq (G α) :=
+  LiftingMagma.instMagmaDecidableEq
+
+theorem MagmaLaw.models_iff_satisfies_ι (law : MagmaLaw α) :
     G α ⊧ law ↔ satisfiesPhi (G := G α) LiftingMagma.ι law := by
   constructor
   · intro h
@@ -21,31 +28,30 @@ theorem MagmaLaw.models_iff_satisfies_ι {α : Type} {G : Type → Type} (law : 
     have := LiftingMagma.lift_factors f
     rw [this, satisfiesPhi.evalHom, h]
 
-section Example
+instance [DecidableEq α] (law : MagmaLaw α) : Decidable (G α ⊧ law) := by
+  rw [MagmaLaw.models_iff_satisfies_ι, satisfiesPhi]
+  infer_instance
 
-@[simp] theorem Fin2.match_eq (X : Sort _) (f : Fin 2 → X) : (fun | 0 => f 0 | 1 => f 1) = f := by
-  funext i
-  match i with
-  | 0 | 1 => rfl
+section Instances
 
-theorem Fin2.uncurry {X : Type _} (P : (Fin 2 → X) → Prop) : (∀ f : Fin 2 → X, P f) ↔
-      ∀ x y : X, P (fun | 0 => x | 1 => y) := by
-  constructor
-  · intro h _ _
-    apply h
-  · intro h f
-    have := h (f 0) (f 1)
-    simpa only [match_eq]
+instance instMagmaList {α : Type _} : Magma (List α) where
+  op := List.append
 
-theorem Fin2.models_iff (G  : Type _) [Magma G] (law : MagmaLaw (Fin 2)) : G ⊧ law ↔
-    ∀ x y : G, satisfiesPhi (fun | 0 => x | 1 => y) law := by
-  rw [satisfies, Fin2.uncurry]
+instance : LiftingMagma List where
+  instMagma := instMagmaList
+  instMagmaDecidableEq := inferInstance
+  ι := fun a ↦ [a]
+  lift f := {
+    toFun := (List.bind · f),
+    map_op' := by
+      intro x y
+      show (x ++ y).bind f = (x.bind f ++ y.bind f)
+      rw [List.bind, List.bind, List.bind, ← List.join_append, List.map_append]
+  }
+  lift_factors := by
+    intro α f
+    funext x
+    symm
+    apply List.bind_singleton
 
-def Law43 : MagmaLaw (Fin 2) := {
-  lhs := (.Leaf 0) ⋆ (.Leaf 1),
-  rhs := (.Leaf 1) ⋆ (.Leaf 0)
-}
-
-example (G  : Type _) [Magma G] : (G ⊧ Law43) ↔ Equation43 G := Fin2.models_iff G Law43
-
-end Example
+end Instances
