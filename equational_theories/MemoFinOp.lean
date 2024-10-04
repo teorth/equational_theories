33ff48f1c2b2d2325005eeb51453dc13e957dbb8
Daniel Weber <commandmaster2017@gmail.com>
2024-10-04 19:33:47 +0300

    Try using power of 2 base

--- equational_theories/MemoFinOp.lean
+++ equational_theories/MemoFinOp.lean
@@ -9,39 +9,41 @@ namespace MemeFinOp
 
 open Lean Meta Elab Term
 
-def opOfTable {n : Nat} (t : Nat) (a : Fin n) (b : Fin n) : Fin n :=
-  let i := a.val * n + b.val
-  let r := (t / n^i) % n
-  ⟨r, Nat.mod_lt _ (Fin.pos a)⟩
+def opOfTable {n : Nat} (s t : Nat) (a : Fin n) (b : Fin n) : Fin n :=
+  ⟨(t >>> (s * (a.val * n + b.val))) % n, Nat.mod_lt _ (Fin.pos a)⟩
 
 /-- `enum n` is the array of all elements of `Fin n` in order -/
-private def Fin.enum (n) : Array (Fin n) := Array.ofFn id
+private def Fin.enum (n) : Array (Fin n) := Array.ofFn Fin.rev
 
-def buildMemo {n : Nat} (f : Fin n → Fin n → Fin n) : Nat := Id.run do
+def buildMemo {n : Nat} (f : Fin n → Fin n → Fin n) : Nat × Nat := Id.run do
+  let s := 1 + Nat.log2 (n-1)
   let mut t := 0
+  let mut last := 0
   for a in Fin.enum n do
     for b in Fin.enum n do
-      let i := a.val * n + b.val
-      t := t + f a b * n^i
-  t
+      let ne := f a b
+      t := (t <<< s) + (ne + n - (last <<< s) % n) % n
+      last := ne
+
+  (t, s)
 
 -- Quick sanity check:
 example :
     let f := fun (a b : Fin 4) => a * a * b + 2
-    opOfTable (buildMemo f) = f := by
+    opOfTable (buildMemo f).2 (buildMemo f).1 = f := by
   funext a b; revert a b; native_decide
 
-private unsafe def evalNatImpl (e : Expr) : MetaM Nat := do
+private unsafe def evalNatProdImpl (e : Expr) : MetaM (Nat × Nat) := do
   let t ← inferType e
-  unless t.isConstOf ``Nat do
-    throwError "evalNat: expected expression of type `Nat`, but got {t}"
+  unless t.isAppOfArity ``Prod 2 && (t.getArg! 1).isConstOf ``Nat && (t.getArg! 2).isConstOf ``Nat do
+    throwError "evalNat: expected expression of type `Nat × Nat`, but got {t}"
   let e ← instantiateMVars e
   if e.hasExprMVar then
     throwError "evalNat: cannot evaluate expression{indentExpr e}\nit contains metavariables"
-  evalExpr Nat (mkConst ``Nat) e
+  evalExpr (Nat × Nat) (← mkAppM ``Prod #[mkConst ``Nat, mkConst ``Nat]) e
 
-@[implemented_by evalNatImpl]
-private opaque evalNat (value : Expr) : MetaM Nat
+@[implemented_by evalNatProdImpl]
+private opaque evalNatProd (value : Expr) : MetaM (Nat × Nat)
 
 /-
 The syntax `memoFinOp f` takes a function `f : Fin n → Fin n → Fin n` and implements it in a
@@ -72,8 +74,8 @@ elab "memoFinOp" fn:term:arg :term <= expectedType? => do
     throwError "expected type {expectedType}, got {type}"
 
   -- Tabulation
-  let table ← evalNat (mkApp2 (mkConst ``buildMemo) nE fn)
-  return mkApp2 (mkConst ``opOfTable) nE (.lit (.natVal table))
+  let table ← evalNatProd (mkApp2 (mkConst ``buildMemo) nE fn)
+  return mkApp3 (mkConst ``opOfTable) nE (.lit (.natVal table.snd)) (.lit (.natVal table.fst))
 
 example :
     let f := fun (a b : Fin 4) => a * a * b + (2 : Fin 4)
