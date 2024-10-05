a67716bf94d827598bb44073295c1af0e3d84796
Daniel Weber <55664973+Command-Master@users.noreply.github.com>
2024-10-05 17:19:58 +0300

    Finite counterexamples generated by the Vampire theorem prover (#276)
    Solves ~8,000 unknown implications
    
    ---------
    
    Co-authored-by: Joachim Breitner <mail@joachim-breitner.de>

--- equational_theories/Generated/VampireProven.lean
+++ equational_theories/Generated/VampireProven.lean
@@ -1,3 +1,11 @@
+import equational_theories.Generated.VampireProven.Disproofs1
+import equational_theories.Generated.VampireProven.Disproofs2
+import equational_theories.Generated.VampireProven.Disproofs3
+import equational_theories.Generated.VampireProven.Disproofs4
+import equational_theories.Generated.VampireProven.Disproofs5
+import equational_theories.Generated.VampireProven.Disproofs6
+import equational_theories.Generated.VampireProven.Disproofs7
+import equational_theories.Generated.VampireProven.Disproofs8
 import equational_theories.Generated.VampireProven.Proofs1
 import equational_theories.Generated.VampireProven.Proofs1
 import equational_theories.Generated.VampireProven.Proofs2
