1f2b19606c96d6550c8d8bbce087e6d09cc5e377
agjftucker <agjftucker@gmail.com>
2024-10-05 02:26:02 +0100

    fix(equational_theories/Preorder.lean): partial revert of #273 "reverse pre-order direction" (#293)
    …rder direction"

--- equational_theories/Preorder.lean
+++ equational_theories/Preorder.lean
@@ -22,7 +22,7 @@ def implies (l₁ l₂ : MagmaLaw α) := ∀ {G : Type} [Magma G],
 If a law `l₁` implies a law `l₂`, then we say `l₁ ≤ l₂`.
 -/
 instance : LE (MagmaLaw α) where
-  le l₁ l₂ := l₂.implies l₁
+  le l₁ l₂ := l₁.implies l₂
 
 /--
 A stronger law is smaller than a weaker law, because this corresponds to the inclusion of
@@ -40,13 +40,13 @@ theorem implies_refl (l : MagmaLaw α) : l ≤ l := fun {G} [Magma G] a ↦ a
 theorem implies_trans {l₁ l₂ l₃ : MagmaLaw α} : l₁ ≤ l₂ → l₂ ≤ l₃ → l₁ ≤ l₃ := by
   intro h₁ h₂ G inst h
   dsimp only [satisfies, satisfiesPhi] at *
-  exact h₁ (h₂ h)
+  exact h₂ (h₁ h)
 
 instance : Preorder (MagmaLaw α) where
   le_refl := implies_refl
   le_trans := fun _ _ _ ↦ implies_trans
 
-theorem implies_eq_singleton_models {l₁ l₂ : MagmaLaw α} : l₁ ≤ l₂ ↔ {l₂} ⊧ l₁ := by
+theorem implies_eq_singleton_models {l₁ l₂ : MagmaLaw α} : l₁ ≤ l₂ ↔ {l₁} ⊧ l₂ := by
   simp only [LE.le, implies, models, satisfiesSet, Ctx, Set.mem_singleton_iff, forall_eq]
 
 end Law.MagmaLaw
