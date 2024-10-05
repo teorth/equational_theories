b287fc32458acb4f09c72fed2fb3f077b9cfb24f
Zoltan A. Kocsis <11808286+zaklogician@users.noreply.github.com>
2024-10-06 06:53:27 +1100

    All4x4Tables: 2531 does not impy 1075 (#330)
    I give a 13x13 counterexample showing that neither of the dual pair
    1076/2531 imply 1075. The constructed magma ends up resolving over 1% of
    the remaining unknowns.
    
    This dual pair had by far the highest unknown count among those
    equations that are not implied by anything else (except for x=y).

--- equational_theories/Generated/All4x4Tables.lean
+++ equational_theories/Generated/All4x4Tables.lean
@@ -738,3 +738,4 @@ import equational_theories.Generated.All4x4Tables.Refutation736
 import equational_theories.Generated.All4x4Tables.Refutation737
 import equational_theories.Generated.All4x4Tables.Refutation738
 import equational_theories.Generated.All4x4Tables.Refutation739
+import equational_theories.Generated.All4x4Tables.Refutation740
