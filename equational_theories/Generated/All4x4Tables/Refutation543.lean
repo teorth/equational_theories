9ad7189e7a9bde39cb466efbc4a675f3ea2f5af2
Zoltan A. Kocsis <11808286+zaklogician@users.noreply.github.com>
2024-10-05 00:01:45 +0000

    All4x4Tables: more hand-curated cancellative magmas
    Adds 4 more cancellative magmas that were found through hand-guided
    search and which refute 31 still-open implications. These were still
    open following the vampire theorems.
    
    The magmas were found by hand-searching for cancellative magmas that
    fail to satisfy large one-variable laws.
    
    As an added benefit, the number of explicit false proofs is reduced
    by a further 1.1k.
    
    {"explicit_proof_true": 28264,
    "implicit_proof_true": 5457961,
    "unknown": 2726273,
    "implicit_proof_false": 13244610,
    "explicit_proof_false": 604701
    }

---
+++ equational_theories/Generated/All4x4Tables/Refutation543.lean
@@ -0,0 +1,22 @@
+
+import equational_theories.AllEquations
+import equational_theories.FactsSyntax
+import equational_theories.MemoFinOp
+import equational_theories.DecideBang
+
+/-!
+This file is generated from the following operator table:
+[[1,2,3,4,5,0],[0,5,4,3,2,1],[5,4,1,0,3,2],[4,1,2,5,0,3],[3,0,5,2,1,4],[2,3,0,1,4,5]]
+-/
+
+set_option linter.unusedVariables false
+
+/-! The magma definition -/
+def «FinitePoly [[1,2,3,4,5,0],[0,5,4,3,2,1],[5,4,1,0,3,2],[4,1,2,5,0,3],[3,0,5,2,1,4],[2,3,0,1,4,5]]» : Magma (Fin 6) where
+  op := memoFinOp fun x y => [[1,2,3,4,5,0],[0,5,4,3,2,1],[5,4,1,0,3,2],[4,1,2,5,0,3],[3,0,5,2,1,4],[2,3,0,1,4,5]][x.val]![y.val]!
+
+/-! The facts -/
+@[equational_result]
+theorem «Facts from FinitePoly [[1,2,3,4,5,0],[0,5,4,3,2,1],[5,4,1,0,3,2],[4,1,2,5,0,3],[3,0,5,2,1,4],[2,3,0,1,4,5]]» :
+  ∃ (G : Type) (_ : Magma G), Facts G [917, 1729, 2541] [411, 614, 1832, 3050] :=
+    ⟨Fin 6, «FinitePoly [[1,2,3,4,5,0],[0,5,4,3,2,1],[5,4,1,0,3,2],[4,1,2,5,0,3],[3,0,5,2,1,4],[2,3,0,1,4,5]]», by decideFin!⟩
