afde66bc4999caff5e8fe029135c216bf65c037a
Daniel Weber <55664973+Command-Master@users.noreply.github.com>
2024-10-06 20:19:32 +0300

    Add conjectures produced by Vampire (#373)
    These conjectures are created from implications which are proven to be
    false by saturation, which we don't know how to formalize in Lean yet.
    Seeing as `Equation3964_not_implies_Equation3456` from #362 was
    originally in here, I don't think an off-by-one situation like occurred
    previously is likely (these are also transitively consistent with all
    existing results).

---
+++ equational_theories/Generated/VampireProven/src/conjectures.py
@@ -0,0 +1,13 @@
+import json, random
+
+f = open(f'equational_theories/Generated/VampireProven/Conjectures.lean', 'w')
+
+print('import equational_theories.AllEquations', file=f)
+print('import equational_theories.Magma', file=f)
+print('import Mathlib.Tactic.TypeStar', file=f)
+
+for line in open('conjectures.txt'):
+  lhs, rhs = map(int, line.strip(' \n()').split(','))
+  print(f'@[equational_result] conjecture Equation{lhs+1}_not_implies_Equation{rhs+1} : ∃ (G: Type) (_: Magma G), Equation{lhs+1} G ∧ ¬ Equation{rhs+1} G', file=f)
+
+f.close()
