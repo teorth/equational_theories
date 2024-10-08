596344fca42207b66f15d68790a7d358620e682a
David Renshaw <dwrenshaw@gmail.com>
2024-10-08 12:22:05 -0400

    prove that eq3555 does not imply eq 3862 (#431)
    This is the last of the missing proofs for "known duals" computed by
    @amirlb.

---
+++ equational_theories/Generated/All4x4Tables/Refutation744.lean
@@ -0,0 +1,22 @@
+
+import equational_theories.AllEquations
+import equational_theories.FactsSyntax
+import equational_theories.MemoFinOp
+import equational_theories.DecideBang
+
+/-!
+This file is generated from the following operator table:
+[[1,2,6,5,3,4,1],[6,1,2,1,1,5,1],[3,2,2,2,6,4,5],[4,1,2,3,3,5,1],[5,1,5,3,4,4,6],[3,5,6,5,4,5,5],[2,1,4,1,6,5,6]]
+-/
+
+set_option linter.unusedVariables false
+
+/-! The magma definition -/
+def «FinitePoly [[1,2,6,5,3,4,1],[6,1,2,1,1,5,1],[3,2,2,2,6,4,5],[4,1,2,3,3,5,1],[5,1,5,3,4,4,6],[3,5,6,5,4,5,5],[2,1,4,1,6,5,6]]» : Magma (Fin 7) where
+  op := memoFinOp fun x y => [[1,2,6,5,3,4,1],[6,1,2,1,1,5,1],[3,2,2,2,6,4,5],[4,1,2,3,3,5,1],[5,1,5,3,4,4,6],[3,5,6,5,4,5,5],[2,1,4,1,6,5,6]][x.val]![y.val]!
+
+/-! The facts -/
+@[equational_result]
+theorem «Facts from FinitePoly [[1,2,6,5,3,4,1],[6,1,2,1,1,5,1],[3,2,2,2,6,4,5],[4,1,2,3,3,5,1],[5,1,5,3,4,4,6],[3,5,6,5,4,5,5],[2,1,4,1,6,5,6]]» :
+  ∃ (G : Type) (_ : Magma G), Facts G [3555] [3862] :=
+    ⟨Fin 7, «FinitePoly [[1,2,6,5,3,4,1],[6,1,2,1,1,5,1],[3,2,2,2,6,4,5],[4,1,2,3,3,5,1],[5,1,5,3,4,4,6],[3,5,6,5,4,5,5],[2,1,4,1,6,5,6]]», by decideFin!⟩
