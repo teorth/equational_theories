3a426a0775854f7a3564bc0f9cc4f27273405ab0
Shreyas <shreyasss94@gmail.com>
2024-10-09 03:55:39 +0200

    Lean4lean workflow added

--- scripts/lean4lean_checker/main.py
+++ scripts/lean4lean_checker/main.py
@@ -1,72 +1,122 @@
 import os
 import sys
 from pathlib import Path
-import git
 import shutil
 import subprocess
-# configuration options
 
-l4l_repo_url = "git@github.com:digama0/lean4lean.git"
-toolchain_filename = Path('lean-toolchain')
-l4l_relpath = "./lean4lean"
-l4l_binpath = "./.lake/build/bin/lean4lean"
-lakePath = Path().home().joinpath(Path(".elan/bin/lake"))
-project_name = "equational_theories"
 
-def main():
-  print("hello")
-  ## construct absolute file paths
-  pwd = Path(os.getcwd())
-  localToolchainPath = pwd.joinpath(toolchain_filename)
-  l4lPath = pwd.joinpath(l4l_relpath)
-  l4ltoolchainPath = l4lPath.joinpath(toolchain_filename)
-  l4lBinPath = l4lPath.joinpath(l4l_binpath)
+class Lean4LeanChecker():
+  def __init__(self) -> None:
+    self.project_name = "equational_theories"
+    self.l4l_repo_url = "git@github.com:digama0/lean4lean.git"
+    self.toolchain_filename = Path('lean-toolchain')
+    self.topPath = Path(os.getcwd())
+    self.l4lSubPath = self.topPath.joinpath("./lean4lean")
+    self.l4lBinPath = self.l4lSubPath.joinpath("./.lake/build/bin/lean4lean")
+    self.lakePath = Path().home().joinpath(Path(".elan/bin/lake"))
+    self.l4lBuildPath = self.l4lSubPath.joinpath('./.lake')
+    self.l4lManifest = self.l4lSubPath.joinpath('./lake-manifest.json')
+    self.repoToolchainPath = self.topPath.joinpath(self.toolchain_filename)
+    self.l4lToolchainPath = self.l4lSubPath.joinpath(self.toolchain_filename)
 
-  ## Delete any existing copy of lean4lean first
-  if l4lPath.exists():
-    shutil.rmtree(l4lPath)
+  # Check if the toolchains of the project and lean4lean clone match
+  def checkToolchainMatch(self):
+    try:
+      if not self.l4lSubPath.exists():
+          raise Exception("lean4lean path does not exist")
+      l4lToolchain = self.l4lToolchainPath.read_text()
+      projectToolchain = self.repoToolchainPath.read_text()
+      return l4lToolchain == projectToolchain
+    except:
+      print("lean4lean is not available yet")
 
-  ## Get a fresh clone of lean4lean
-  print("Cloning a fresh copy of lean4lean...")
-  repo = git.Repo.clone_from(l4l_repo_url, l4lPath)
+  def try_update_l4l_toolchain(self):
+    try:
+      if not self.l4lSubPath.exists():
+        raise Exception("lean4lean path not found")
+      if self.checkToolchainMatch():
+        print("The project's lean toolchain matches the toolchain of lean4lean")
+        return
+      else:
+        print("The project's toolchain and lean toolchain are different\n")
+        print("An attempt to update lean4lean to a matching toolchain will now begin")
+        # switch to lean4lean directory
+        os.chdir(self.l4lSubPath)
 
-  ##Compare toolchains
-  localtoolchain = localToolchainPath.read_text()
-  l4l_toolchain = l4ltoolchainPath.read_text()
+        # remove lake-manifest.json and ./.lake for lean4lean
+        os.remove(self.l4lManifest)
+        shutil.rmtree(self.l4lBuildPath)
 
-  ## If toolchains don't match, exit without error, but message to contact maintainers
-  if localtoolchain != l4l_toolchain:
-    print("The local toolchain doesn't match the lean4lean toolchain")
-    print("Contact the maintainers")
-    exit(0)
-  else:
-    print("Toolchains match!")
-    print("Lean4lean will be built next")
-  ## Otherwise build lean4lean
-  os.chdir(l4lPath)
-  buildret = subprocess.run([lakePath.as_posix(), "build", "lean4lean"]).returncode
+        #get the project toolchain
+        projectToolchain = self.repoToolchainPath.read_text()
 
-  ## If build doesn't work, quit without error but contact maintainers
-  if buildret != 0:
-    print("lean4lean build failed. Lean4lean will not be run. Contact the maintainers")
-    exit(0)
+        #write it to the lean-toolchain fiel of lean4lean
+        self.l4lToolchainPath.write_text(projectToolchain)
 
-  ## check if build is successful and run lean4lean on the project
-  if Path.exists(l4lBinPath):
-    print("Successfully built lean4lean CLI")
-    print("Starting lean4lean check on the project")
-    os.chdir(pwd)
-    ret = subprocess.run(["env", "LEAN_NUM_THREADS=2",lakePath, "env", l4lBinPath.as_posix(), project_name]).returncode
-    print("lean4lean process returned ", ret)
-    if ret == 0:
-      print("The project is fine. Cleaning up")
-    shutil.rmtree(l4lPath)
-    sys.exit(ret)
-  else:
-    print("lean4lean build unsuccessful")
-    sys.exit(1)
-  #Delete lean4lean
+        #run lake update
+        val = subprocess.run([self.lakePath.as_posix(), "update"]).returncode
+        if val == 0:
+          print("Lean4lean's toolchain has been successfully updated")
+          return
+        else:
+          print("There is a toolchain mismatch and lake cannot update lean4lean")
 
+    except Exception:
+      print("Failed to upgrade lean4lean.\n Cause: {Exception}\nCancelling lean4lean check and exiting")
+      exit(0)
+
+  def remove_old_l4l(self):
+    try:
+      print("Removing any existing lean4lean clones\n")
+      os.chdir(self.topPath)
+      if self.l4lSubPath.exists():
+        shutil.rmtree(self.l4lSubPath)
+    except:
+      print("Failed to remove old copies of lean4lean. The checker will not be run")
+      sys.exit(0)
+
+  def clone_l4l(self):
+    try:
+      subprocess.run(["git","clone", self.l4l_repo_url, self.l4lSubPath])
+    except:
+      print("Failed to clone lean4lean. The checker will not be run")
+      sys.exit(0)
+
+  def build_l4l(self):
+    try:
+      print("Building lean4lean CLI")
+      os.chdir(self.l4lSubPath)
+      subprocess.run([self.lakePath.as_posix(), "build", "lean4lean"])
+    except:
+      print("Error during build of lean4lean cli\n")
+      print("Lean4lean check will exit gracefully")
+      sys.exit(0)
+
+  def run(self):
+    print("Lean4lean check action begins:\n")
+    os.chdir(self.topPath)
+    self.remove_old_l4l()
+    self.clone_l4l()
+    self.try_update_l4l_toolchain()
+    self.build_l4l()
+
+    os.chdir(self.topPath)
+    print("Lean4lean check begins!\n")
+    check_ret = subprocess.run([
+      "env",
+      "LEAN_NUM_THREADS=2",
+      self.lakePath,
+      "env",
+      self.l4lBinPath.as_posix(),
+      self.project_name]).returncode
+    if check_ret == 0:
+      print("Lean4lean check was successful!!")
+      self.remove_old_l4l()
+      sys.exit(0)
+    else:
+      self.remove_old_l4l()
+      sys.exit(1)
 
 if __name__ == "__main__":
-  main()
+  checker = Lean4LeanChecker()
+  checker.run()
