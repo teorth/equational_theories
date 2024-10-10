22ade98a1e2f092f348cbfb29475cc05ad652452
0art0 <18333981+0art0@users.noreply.github.com>
2024-10-10 11:03:40 +0100

    Fix `export_equations` script

--- scripts/export_equations.lean
+++ scripts/export_equations.lean
@@ -3,30 +3,13 @@ import equational_theories.AllEquations
 
 open Lean Elab Command Meta
 
-/-!
-
-# Exporting magma equations
-
-This script exports all the magma equations defined in the `Equations` and `AllEquations` files
-to the JSON file `data/magma_equations.json`.
-
-The format is
-```
-{
-  "equation": "EquationName",
-  "law": {
-    "lhs": { ... },
-    "rhs": { ... }
-  }
-}
--/
-
 def main : IO Unit := do
   searchPathRef.set compile_time_search_path%
   let env ← importModules #[
     { module := `equational_theories.Equations },
     { module := `equational_theories.AllEquations }] .empty
-  let laws := magmaLawExt.getState env
+  let laws ← EIO.toIO (fun _ ↦ IO.userError "Failed to get magma laws.") <|
+            getMagmaLaws.run' { fileName := "", fileMap := default } { env := env }
   let jsonOutput : Json := Json.arr <| laws.map fun ⟨lawName, law⟩ => .mkObj [
     ("equation", magmaLawNameToEquationName lawName.toString),
     ("law", ToJson.toJson law)
