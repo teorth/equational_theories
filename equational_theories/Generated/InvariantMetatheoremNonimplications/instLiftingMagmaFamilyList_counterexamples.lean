a7a0025afae9c0fe280337effbb7a1133cdfffa0
0art0 <18333981+0art0@users.noreply.github.com>
2024-10-11 21:27:03 +0100

    Counterexample files

--- equational_theories/Generated/InvariantMetatheoremNonimplications/instLiftingMagmaFamilyList_counterexamples.lean
+++ equational_theories/Generated/InvariantMetatheoremNonimplications/instLiftingMagmaFamilyList_counterexamples.lean
@@ -2,94 +2,122 @@ import equational_theories.LiftingMagmaFamilies
 import equational_theories.EquationalResult
 import equational_theories.Equations.All
 
-
+@[equational_result]
+theorem Equation1_not_implies_Equation26302 : ∃ (G : Type) (_ : Magma G), Equation1 G ∧ ¬ Equation26302 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation1.models_iff Equation26302.models_iff
 
 @[equational_result]
-conjecture Equation1_not_implies_Equation26302 : ∃ (G : Type _) (_ : Magma G), Equation1 G ∧ ¬Equation26302 G
+theorem Equation1_not_implies_Equation5105 : ∃ (G : Type) (_ : Magma G), Equation1 G ∧ ¬ Equation5105 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation1.models_iff Equation5105.models_iff
 
 @[equational_result]
-conjecture Equation1_not_implies_Equation5105 : ∃ (G : Type _) (_ : Magma G), Equation1 G ∧ ¬Equation5105 G
+theorem Equation1_not_implies_Equation374794 : ∃ (G : Type) (_ : Magma G), Equation1 G ∧ ¬ Equation374794 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation1.models_iff Equation374794.models_iff
 
 @[equational_result]
-conjecture Equation1_not_implies_Equation374794 : ∃ (G : Type _) (_ : Magma G), Equation1 G ∧ ¬Equation374794 G
+theorem Equation1_not_implies_Equation345169 : ∃ (G : Type) (_ : Magma G), Equation1 G ∧ ¬ Equation345169 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation1.models_iff Equation345169.models_iff
 
 @[equational_result]
-conjecture Equation1_not_implies_Equation345169 : ∃ (G : Type _) (_ : Magma G), Equation1 G ∧ ¬Equation345169 G
+theorem Equation1_not_implies_Equation28770 : ∃ (G : Type) (_ : Magma G), Equation1 G ∧ ¬ Equation28770 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation1.models_iff Equation28770.models_iff
 
 @[equational_result]
-conjecture Equation1_not_implies_Equation28770 : ∃ (G : Type _) (_ : Magma G), Equation1 G ∧ ¬Equation28770 G
+theorem Equation4512_not_implies_Equation26302 : ∃ (G : Type) (_ : Magma G), Equation4512 G ∧ ¬ Equation26302 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4512.models_iff Equation26302.models_iff
 
 @[equational_result]
-conjecture Equation4512_not_implies_Equation26302 : ∃ (G : Type _) (_ : Magma G), Equation4512 G ∧ ¬Equation26302 G
+theorem Equation4512_not_implies_Equation5105 : ∃ (G : Type) (_ : Magma G), Equation4512 G ∧ ¬ Equation5105 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4512.models_iff Equation5105.models_iff
 
 @[equational_result]
-conjecture Equation4512_not_implies_Equation5105 : ∃ (G : Type _) (_ : Magma G), Equation4512 G ∧ ¬Equation5105 G
+theorem Equation4512_not_implies_Equation374794 : ∃ (G : Type) (_ : Magma G), Equation4512 G ∧ ¬ Equation374794 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4512.models_iff Equation374794.models_iff
 
 @[equational_result]
-conjecture Equation4512_not_implies_Equation374794 : ∃ (G : Type _) (_ : Magma G), Equation4512 G ∧ ¬Equation374794 G
+theorem Equation4512_not_implies_Equation345169 : ∃ (G : Type) (_ : Magma G), Equation4512 G ∧ ¬ Equation345169 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4512.models_iff Equation345169.models_iff
 
 @[equational_result]
-conjecture Equation4512_not_implies_Equation345169 : ∃ (G : Type _) (_ : Magma G), Equation4512 G ∧ ¬Equation345169 G
+theorem Equation4512_not_implies_Equation28770 : ∃ (G : Type) (_ : Magma G), Equation4512 G ∧ ¬ Equation28770 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4512.models_iff Equation28770.models_iff
 
 @[equational_result]
-conjecture Equation4512_not_implies_Equation28770 : ∃ (G : Type _) (_ : Magma G), Equation4512 G ∧ ¬Equation28770 G
+theorem Equation4396_not_implies_Equation26302 : ∃ (G : Type) (_ : Magma G), Equation4396 G ∧ ¬ Equation26302 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4396.models_iff Equation26302.models_iff
 
 @[equational_result]
-conjecture Equation4396_not_implies_Equation26302 : ∃ (G : Type _) (_ : Magma G), Equation4396 G ∧ ¬Equation26302 G
+theorem Equation4396_not_implies_Equation5105 : ∃ (G : Type) (_ : Magma G), Equation4396 G ∧ ¬ Equation5105 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4396.models_iff Equation5105.models_iff
 
 @[equational_result]
-conjecture Equation4396_not_implies_Equation5105 : ∃ (G : Type _) (_ : Magma G), Equation4396 G ∧ ¬Equation5105 G
+theorem Equation4396_not_implies_Equation374794 : ∃ (G : Type) (_ : Magma G), Equation4396 G ∧ ¬ Equation374794 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4396.models_iff Equation374794.models_iff
 
 @[equational_result]
-conjecture Equation4396_not_implies_Equation374794 : ∃ (G : Type _) (_ : Magma G), Equation4396 G ∧ ¬Equation374794 G
+theorem Equation4396_not_implies_Equation345169 : ∃ (G : Type) (_ : Magma G), Equation4396 G ∧ ¬ Equation345169 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4396.models_iff Equation345169.models_iff
 
 @[equational_result]
-conjecture Equation4396_not_implies_Equation345169 : ∃ (G : Type _) (_ : Magma G), Equation4396 G ∧ ¬Equation345169 G
+theorem Equation4396_not_implies_Equation28770 : ∃ (G : Type) (_ : Magma G), Equation4396 G ∧ ¬ Equation28770 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4396.models_iff Equation28770.models_iff
 
 @[equational_result]
-conjecture Equation4396_not_implies_Equation28770 : ∃ (G : Type _) (_ : Magma G), Equation4396 G ∧ ¬Equation28770 G
+theorem Equation4380_not_implies_Equation26302 : ∃ (G : Type) (_ : Magma G), Equation4380 G ∧ ¬ Equation26302 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4380.models_iff Equation26302.models_iff
 
 @[equational_result]
-conjecture Equation4380_not_implies_Equation26302 : ∃ (G : Type _) (_ : Magma G), Equation4380 G ∧ ¬Equation26302 G
+theorem Equation4380_not_implies_Equation5105 : ∃ (G : Type) (_ : Magma G), Equation4380 G ∧ ¬ Equation5105 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4380.models_iff Equation5105.models_iff
 
 @[equational_result]
-conjecture Equation4380_not_implies_Equation5105 : ∃ (G : Type _) (_ : Magma G), Equation4380 G ∧ ¬Equation5105 G
+theorem Equation4380_not_implies_Equation374794 : ∃ (G : Type) (_ : Magma G), Equation4380 G ∧ ¬ Equation374794 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4380.models_iff Equation374794.models_iff
 
 @[equational_result]
-conjecture Equation4380_not_implies_Equation374794 : ∃ (G : Type _) (_ : Magma G), Equation4380 G ∧ ¬Equation374794 G
+theorem Equation4380_not_implies_Equation345169 : ∃ (G : Type) (_ : Magma G), Equation4380 G ∧ ¬ Equation345169 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4380.models_iff Equation345169.models_iff
 
 @[equational_result]
-conjecture Equation4380_not_implies_Equation345169 : ∃ (G : Type _) (_ : Magma G), Equation4380 G ∧ ¬Equation345169 G
+theorem Equation4380_not_implies_Equation28770 : ∃ (G : Type) (_ : Magma G), Equation4380 G ∧ ¬ Equation28770 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4380.models_iff Equation28770.models_iff
 
 @[equational_result]
-conjecture Equation4380_not_implies_Equation28770 : ∃ (G : Type _) (_ : Magma G), Equation4380 G ∧ ¬Equation28770 G
+theorem Equation4435_not_implies_Equation26302 : ∃ (G : Type) (_ : Magma G), Equation4435 G ∧ ¬ Equation26302 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4435.models_iff Equation26302.models_iff
 
 @[equational_result]
-conjecture Equation4435_not_implies_Equation26302 : ∃ (G : Type _) (_ : Magma G), Equation4435 G ∧ ¬Equation26302 G
+theorem Equation4435_not_implies_Equation5105 : ∃ (G : Type) (_ : Magma G), Equation4435 G ∧ ¬ Equation5105 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4435.models_iff Equation5105.models_iff
 
 @[equational_result]
-conjecture Equation4435_not_implies_Equation5105 : ∃ (G : Type _) (_ : Magma G), Equation4435 G ∧ ¬Equation5105 G
+theorem Equation4435_not_implies_Equation374794 : ∃ (G : Type) (_ : Magma G), Equation4435 G ∧ ¬ Equation374794 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4435.models_iff Equation374794.models_iff
 
 @[equational_result]
-conjecture Equation4435_not_implies_Equation374794 : ∃ (G : Type _) (_ : Magma G), Equation4435 G ∧ ¬Equation374794 G
+theorem Equation4435_not_implies_Equation345169 : ∃ (G : Type) (_ : Magma G), Equation4435 G ∧ ¬ Equation345169 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4435.models_iff Equation345169.models_iff
 
 @[equational_result]
-conjecture Equation4435_not_implies_Equation345169 : ∃ (G : Type _) (_ : Magma G), Equation4435 G ∧ ¬Equation345169 G
+theorem Equation4435_not_implies_Equation28770 : ∃ (G : Type) (_ : Magma G), Equation4435 G ∧ ¬ Equation28770 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4435.models_iff Equation28770.models_iff
 
 @[equational_result]
-conjecture Equation4435_not_implies_Equation28770 : ∃ (G : Type _) (_ : Magma G), Equation4435 G ∧ ¬Equation28770 G
+theorem Equation4473_not_implies_Equation26302 : ∃ (G : Type) (_ : Magma G), Equation4473 G ∧ ¬ Equation26302 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4473.models_iff Equation26302.models_iff
 
 @[equational_result]
-conjecture Equation4473_not_implies_Equation26302 : ∃ (G : Type _) (_ : Magma G), Equation4473 G ∧ ¬Equation26302 G
+theorem Equation4473_not_implies_Equation5105 : ∃ (G : Type) (_ : Magma G), Equation4473 G ∧ ¬ Equation5105 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4473.models_iff Equation5105.models_iff
 
 @[equational_result]
-conjecture Equation4473_not_implies_Equation5105 : ∃ (G : Type _) (_ : Magma G), Equation4473 G ∧ ¬Equation5105 G
+theorem Equation4473_not_implies_Equation374794 : ∃ (G : Type) (_ : Magma G), Equation4473 G ∧ ¬ Equation374794 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4473.models_iff Equation374794.models_iff
 
 @[equational_result]
-conjecture Equation4473_not_implies_Equation374794 : ∃ (G : Type _) (_ : Magma G), Equation4473 G ∧ ¬Equation374794 G
+theorem Equation4473_not_implies_Equation345169 : ∃ (G : Type) (_ : Magma G), Equation4473 G ∧ ¬ Equation345169 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4473.models_iff Equation345169.models_iff
 
 @[equational_result]
-conjecture Equation4473_not_implies_Equation345169 : ∃ (G : Type _) (_ : Magma G), Equation4473 G ∧ ¬Equation345169 G
-
-@[equational_result]
-conjecture Equation4473_not_implies_Equation28770 : ∃ (G : Type _) (_ : Magma G), Equation4473 G ∧ ¬Equation28770 G
+theorem Equation4473_not_implies_Equation28770 : ∃ (G : Type) (_ : Magma G), Equation4473 G ∧ ¬ Equation28770 G :=
+    LiftingMagmaFamily.establishNonimplication (family := instLiftingMagmaFamilyList) _ Equation4473.models_iff Equation28770.models_iff
