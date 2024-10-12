ecdf9e796f03f020ddf47fd9024b3acc4b9cbec5
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2024-10-12 22:39:23 +0200

    golfing

--- equational_theories/FreeMagma.lean
+++ equational_theories/FreeMagma.lean
@@ -102,11 +102,11 @@ theorem fmapHom_id {α} (m : FreeMagma α) : fmapHom id m = m := evalInMagma_lea
    intros g glift
    let rec equiv : ∀ tx : FreeMagma α, evalInMagma f tx = g.toFun tx := fun tx ↦
       match tx with
-      | FreeMagma.Leaf x => Eq.symm $ congrFun glift x
+      | FreeMagma.Leaf x => (congrFun glift x).symm
       | FreeMagma.Fork txleft txright => Eq.trans
          (congrArg (fun t ↦ t ◇ evalInMagma f txright) (equiv txleft)) $ Eq.trans
          (congrArg (fun t ↦ g.toFun txleft ◇ t) (equiv txright))
-         (Eq.symm $ g.map_op' txleft txright)
+         (g.map_op' txleft txright).symm
    exact (funext equiv)
 
  theorem FmapFreeMagmaUniversalProperty {α : Type u} {β : Type u} (f : α → β)
