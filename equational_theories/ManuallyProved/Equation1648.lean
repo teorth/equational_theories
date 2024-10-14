3b6045d55008ef58411f9b6a4bb7d853ab85d4bf
Vlad Tsyrklevich <vlad@tsyrklevich.net>
2024-10-14 18:20:46 +0200

    Add another dozen refutations for Equation1648 (#570) (#574)
    null
    
    Closes #570

--- equational_theories/ManuallyProved/Equation1648.lean
+++ equational_theories/ManuallyProved/Equation1648.lean
@@ -1,10 +1,14 @@
+-- https://leanprover.zulipchat.com/#narrow/stream/458659-Equational/topic/1648.20!.3D.3E.20206/near/476775769
+
 import Mathlib.Tactic
-import equational_theories.Equations.Eqns1_999
-import equational_theories.Equations.Eqns1000_1999
+import equational_theories.EquationalResult
+import equational_theories.Equations.All
+import equational_theories.FactsSyntax
 
 def sign (x : ℤ) :=
   if x = 0 then 0 else if x < 0 then -1 else 1
 
+@[equational_result]
 theorem Equation1648_not_implies_Equation206 : ∃ (G: Type) (_: Magma G),
   Equation1648 G ∧ ¬ Equation206 G := by
   let instMagmaInt : Magma ℤ := {
@@ -24,18 +28,21 @@ theorem Equation1648_not_implies_Equation206 : ∃ (G: Type) (_: Magma G),
       Left.neg_neg_iff, zero_lt_one, sub_neg_eq_add, zero_add, Int.reduceLT, zero_sub, sub_self,
       zero_eq_neg, not_false_eq_true, instMagmaInt]
 
-theorem Equation1648_not_implies_Equation151 : ∃ (G: Type) (_: Magma G),
-  Equation1648 G ∧ ¬ Equation151 G := by
+@[equational_result]
+theorem Equation1648_facts : ∃ (G : Type) (_ : Magma G), Facts G [1648] [151, 203, 307, 1426, 1832, 2238, 2441, 3050, 3456, 3522, 4065] := by
   let instMagmaInt : Magma ℤ := {
     op := fun x y => if x > y then x + 1 else x - 1
   }
   use ℤ, instMagmaInt
-  simp only [Equation1648, Equation151, not_forall]
 
   constructor
   · intro x y
     simp only [gt_iff_lt, instMagmaInt]
     split <;> simp <;> split <;> simp_all <;> linarith
-  · simp only [gt_iff_lt, lt_self_iff_false, ↓reduceIte, instMagmaInt]
-    use 0
-    simp
+  · repeat' apply And.intro
+    all_goals {
+      by_contra h
+      try specialize h 0 1
+      try specialize h 0
+      simp [instMagmaInt] at h
+    }
