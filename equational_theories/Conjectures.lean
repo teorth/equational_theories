8278c5b9652f3ccadf959fd246766c354cc056fb
Daniel Weber <55664973+Command-Master@users.noreply.github.com>
2024-10-15 20:58:31 +0300

    Add conjectures from greedy construction (#594)

--- equational_theories/Conjectures.lean
+++ equational_theories/Conjectures.lean
@@ -41,5 +41,44 @@ conjecture Equation229_facts : ∃ (G : Type) (_ : Magma G), Facts G [229] [73]
 @[equational_result]
 conjecture Equation1491_facts : ∃ (G : Type) (_ : Magma G), Facts G [1491] [65]
 
+/-- See https://leanprover.zulipchat.com/#narrow/stream/458659-Equational/topic/1076.20!.3D.3E.203/near/477009119 -/
+@[equational_result]
+conjecture Equation118_facts : ∃ (G : Type) (_ : Magma G), Facts G [118] [222, 274, 4435]
+
+@[equational_result]
+conjecture Equation124_facts : ∃ (G : Type) (_ : Magma G), Facts G [124] [1924]
+
+@[equational_result]
+conjecture Equation476_facts : ∃ (G : Type) (_ : Magma G), Facts G [476] [359, 3862, 4065]
+
+@[equational_result]
+conjecture Equation503_facts : ∃ (G : Type) (_ : Magma G), Facts G [503] [359, 3862, 4065]
+
+@[equational_result]
+conjecture Equation677_facts : ∃ (G : Type) (_ : Magma G), Facts G [677] [255]
+
+@[equational_result]
+conjecture Equation707_facts : ∃ (G : Type) (_ : Magma G), Facts G [707] [1223, 1316, 2238, 2847, 2940]
+
+@[equational_result]
+conjecture Equation713_facts : ∃ (G : Type) (_ : Magma G), Facts G [713] [359, 817, 1426, 3862, 4065]
+
+@[equational_result]
+conjecture Equation883_facts : ∃ (G : Type) (_ : Magma G), Facts G [883] [2035, 2101, 2238, 2304]
+
+@[equational_result]
+conjecture Equation906_facts : ∃ (G : Type) (_ : Magma G), Facts G [906] [3862]
+
+@[equational_result]
+conjecture Equation1112_facts : ∃ (G : Type) (_ : Magma G), Facts G [1112] [8, 411, 1629, 1832, 3253, 3319, 3862, 3915]
+
+@[equational_result]
+conjecture Equation1113_facts : ∃ (G : Type) (_ : Magma G), Facts G [1113] [2441, 2534]
+
+@[equational_result]
+conjecture Equation1289_facts : ∃ (G : Type) (_ : Magma G), Facts G [1289] [2507, 3116, 4435]
+
+@[equational_result]
+conjecture Equation1447_facts : ∃ (G : Type) (_ : Magma G), Facts G [1447] [1431, 4269]
 
 end Conjectures
