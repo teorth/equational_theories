2b7e1178ec06cb220c6a7aa16cf329fa8cdcf306
Alex Meiburg <timeroot.alex@gmail.com>
2024-10-17 18:31:20 -0400

    Draft of Obelix =!=> Asterix work

---
+++ equational_theories/Obelix.lean
@@ -0,0 +1,209 @@
+import Mathlib.Algebra.Order.Ring.Nat
+import Mathlib.Data.Finsupp.Notation
+import Mathlib.Data.ZMod.Defs
+import Mathlib.Logic.Denumerable
+import Mathlib.Tactic
+
+import equational_theories.FactsSyntax
+import equational_theories.EquationalResult
+import equational_theories.Equations.Basic
+
+-- The ``Obelix law''
+-- equation 1491 := x = (y ◇ x) ◇ (y ◇ (y ◇ x))
+
+namespace Obelix
+
+
+--The particular group that we'll work on: (ℕ×ℕ)-indexed functions to ℤ with finite support.
+--The ℕ×ℕ lets us easily get "fresh" generators to keep extending the function. Finite support means
+--that the group is still countable, so we can denumerate every element and eventually add it
+--to the domain. We could easily use ℚ or Fin p instead of ℤ if we wanted.
+--Significant amounts of the construction -- even defining the invariants of the partial function --
+--depend on this, so we use it explicitly instead of making PartialSolution depend on a group G.
+abbrev A : Type := (ℕ × ℕ) →₀ ℤ
+
+noncomputable instance A_group : AddCommGroup A := Finsupp.instAddCommGroup
+
+@[ext]
+structure PartialSolution where
+  --A partial solution is a function f : A → A satisfying certain invariants, with support Supp.
+  Supp : Set A
+  f : Supp → A
+  --If x is in the domain, so is f(x).
+  Closed_f : ∀ {a}, (ha : a ∈ Supp) → f ⟨a,ha⟩ ∈ Supp
+  --If x is in the domain, so is f (f x) - f(x).
+  Closed_sub : ∀ {a}, (ha : a ∈ Supp) → f ⟨_, Closed_f ha⟩ - f ⟨a,ha⟩ ∈ Supp
+  --For all x where it's defined, f( f(f(x)) - f(x) ) = x - f(x).
+  Valid : ∀ {a}, (ha : a ∈ Supp) → f ⟨_, Closed_sub ha⟩ = a - f ⟨_, Closed_f ha⟩
+  --There are nats n and m such that...
+  n : ℕ
+  m : ℕ
+  --the Supp is empty for any (a : A) with support on a (i,j) where i > n.
+  noSuppN : ∀ (a : A), (∃ i j, n < i ∧ a (i,j) ≠ 0) → a ∉ Supp
+  --and for n = i, it's empty whenever j > m.
+  noSuppM : ∀ (a : A), (∃ j, m < j ∧ a (n,j) ≠ 0) → a ∉ Supp
+
+namespace PartialSolution
+
+/-- The bifurcation tree of new elements to add to solution, defined as a ℕ sequence of (x,f(x)) pairs.
+The encoding is seq 0 for the root (a,·), and node i has children 2i+1 and 2i+2 for entries
+ (f(x),·) and (f(f(x))-f(x),·) respectively.
+-/
+noncomputable def bifurcationTree (f : PartialSolution) {a : A} (ha : a ∉ f.Supp) (n : ℕ) : A × A :=
+  if n = 0 then
+    (a, fun₀ | (f.n, f.m+n+1) => 1)
+  else if Odd n then
+    let i := (n-1)/2;
+    let x := f.bifurcationTree ha i;
+    (x.2, fun₀ | (f.n, f.m+n+1) => 1)
+  else
+    let i := (n-2)/2;
+    let x := f.bifurcationTree ha i;
+    let fx := f.bifurcationTree ha (n-1);
+    (fx.2 - x.2, x.1 - x.2)
+
+/-- The elements in the bifurcation tree do not overlap with the existing domain. -/
+theorem bifurcationTreeDisjoint (f : PartialSolution) {a : A} (ha : a ∉ f.Supp) (n : ℕ) :
+    (f.bifurcationTree ha n).1 ∉ f.Supp := by
+  sorry
+
+/-- The elements in the bifurcation tree never have the same domain, the same fst. -/
+theorem bifurcationTreeUnique (f : PartialSolution) {a : A} (ha : a ∉ f.Supp) (n₁ n₂ : ℕ) (hn : n₁ ≠ n₂) :
+    (f.bifurcationTree ha n₁).1 ≠ (f.bifurcationTree ha n₂).1 := by
+  sorry
+
+--Something like this must already exist.
+def combineSetFuncs {α β : Type*} {s t : Set α} [DecidablePred (· ∈ s)] (f : s → β) (g : t → β) :
+    (s.union t) → β :=
+  fun ⟨x,hx⟩ ↦ if h₂ : x ∈ s then f ⟨x, h₂⟩ else g ⟨x, Or.resolve_left hx h₂⟩
+
+open Classical in
+/-- Extend a partial solution with an element not in its support, adding the full bifurcation tree. -/
+noncomputable def add (f : PartialSolution) {x : A} (hx : x ∉ f.Supp) : PartialSolution where
+  Supp := f.Supp ∪ Set.range (fun n ↦ (f.bifurcationTree hx n).1)
+  f := combineSetFuncs f.f (fun x ↦ (f.bifurcationTree hx (Nat.find x.2)).2)
+  Closed_f := by
+    rintro a (h|h)
+    · apply Or.inl
+      simp only [combineSetFuncs, dif_pos h]
+      apply f.Closed_f
+    · apply Or.inr
+      obtain ⟨n,hn⟩ := h
+      have := f.bifurcationTreeDisjoint hx n
+      simp only [hn] at this
+      simp only [combineSetFuncs, dif_neg this, Set.mem_range]
+      use 2*n + 1
+      sorry
+  Closed_sub := by
+    rintro a (h|h)
+    · apply Or.inl
+      simp [combineSetFuncs, dif_pos h, dif_pos (f.Closed_f h)]
+      apply f.Closed_sub
+    · apply Or.inr
+      obtain ⟨n,hn⟩ := h
+      have := (f.bifurcationTreeDisjoint hx n)
+      simp only [hn] at this
+      simp only [combineSetFuncs, dif_neg this, Set.mem_range]
+      use 2*n + 2
+      sorry
+  Valid := by
+    sorry
+  n := f.n+1
+  m := 0
+  noSuppN := by
+    rintro a ⟨i,j,hi,ha⟩
+    simp only [Set.mem_union, Set.mem_range, not_or, not_exists]
+    constructor
+    · exact f.noSuppN a ⟨i, j, by linarith, ha⟩
+    · intro x
+      sorry
+  noSuppM := by
+    rintro a ⟨j,hi,ha⟩
+    simp only [Set.mem_union, Set.mem_range, not_or, not_exists]
+    constructor
+    · exact f.noSuppN a ⟨f.n+1, j, by linarith, ha⟩
+    · intro x
+      sorry
+
+/-- The extended partial solution has the new element in the support. -/
+theorem add_supp (f : PartialSolution) {x : A} (hx : x ∉ f.Supp) : x ∈ (f.add hx).Supp :=
+  Or.inr ⟨0, by simp only [bifurcationTree, ↓reduceIte]⟩
+
+/-- The extended partial solution includes the old support. -/
+theorem extends_supp (f : PartialSolution) {x y : A} (hx : x ∉ f.Supp) (hy : y ∈ f.Supp) :
+    y ∈ (f.add hx).Supp :=
+  Or.inl hy
+
+/-- The extended partial solution agrees with the old partial solution on its domain. -/
+theorem agrees_supp (f : PartialSolution) {x y : A} (hx : x ∉ f.Supp) (hy : y ∈ f.Supp) :
+    f.f ⟨y,hy⟩ = (f.add hx).f ⟨y, f.extends_supp hx hy⟩ := by
+  simp [add, combineSetFuncs, dif_pos hy]
+
+open Classical in
+/-- Pick the next element to add, using a denumeration of everything in the group. -/
+noncomputable def nextElemToAdd (f : PartialSolution) : {x : A // x ∉ f.Supp} := by
+  have Pn : ∃ n : ℕ, ∃ x : {x : A // x ∉ f.Supp}, (Encodable.decode n) = some x := by
+    obtain ⟨x,hx⟩ : ∃ x : A, x ∉ f.Supp :=
+       ⟨fun₀ | (f.n+1, 0) => 1, f.noSuppN _ ⟨f.n+1, 0, by norm_num⟩⟩
+    use Encodable.encode (α := {x : A // x ∉ f.Supp}) ⟨x, hx⟩, ⟨x, hx⟩
+    exact Encodable.encodek _
+  let k : Option {x : A // x ∉ f.Supp} := Encodable.decode (Nat.find Pn)
+  use k.get ?_
+  all_goals (
+    obtain ⟨x, hx1⟩ := Nat.find_spec Pn
+    simp [k, hx1, x.2]
+  )
+
+/-- Repeatedly extend f by the least element not in its domain, and the bifurcation tree that element
+  generates. -/
+noncomputable def closureSeq (f : PartialSolution) : ℕ → PartialSolution
+| 0 => f
+| n+1 => (closureSeq f n).add (closureSeq f n).nextElemToAdd.2
+
+/-- All elements are eventually in the closure. -/
+theorem closureSeq_eventually_total (f : PartialSolution) (x : A) :
+    ∃n, x ∈ (f.closureSeq n).Supp := by
+  sorry
+
+open Classical in
+/-- Make the linearizing function f from the closure. -/
+noncomputable def closureLinear (f : PartialSolution) : A → A :=
+  fun a ↦ (f.closureSeq (Nat.find (f.closureSeq_eventually_total a))).f
+    ⟨a, Nat.find_spec (f.closureSeq_eventually_total a)⟩
+
+/-- The linearizing function satisfies the functional equation, f( f(f(x)) - f(x) ) = x - f(x). -/
+theorem closureLinear_funeq (f₀ : PartialSolution) :
+    let f := closureLinear f₀; ∀ x, f (f (f x) - f x) = x - f x := by
+  sorry
+
+/-- The linearizing function agrees with the initial PartialSolution on its support. -/
+theorem closureLinear_extends (f₀ : PartialSolution) :
+    ∀ x, (h : x ∈ f₀.Supp) → closureLinear f₀ x = f₀.f ⟨x,h⟩ := by
+  sorry
+
+/-- Define the magma from the linearizing function. -/
+noncomputable def closure (f : PartialSolution) : A → A → A :=
+  fun a b ↦ a + (closureLinear f) (b - a)
+
+/-- The resulting magma obeys the Obelix rule. -/
+theorem closure_prop (f : PartialSolution) : ∀ x y, x = closure f (closure f y x) (closure f y (closure f y x)) :=
+  fun x y ↦ by simp [closure, closureLinear_funeq f (x - y)]
+
+/-- An initial solution -/
+def initial : PartialSolution := --where
+  -- Supp := {0, fun₀ | 1 => 1, fun₀ | 1 => 1 | 2 => 1, fun₀ | 1 => 1 | 3 => 1}
+  -- f a b :=
+  --   if a = 0 then 0 else
+  --   if a = fun₀ | 1 => 1 then fun₀ | 2 => 1 else
+  --   if a = fun₀ | 1 => 1 | 2 => 1 then fun₀ | 3 => 1 else
+  --   if a = fun₀ | 1 => 1 | 3 => 1 then fun₀ | 4 => 1 else
+  --   0
+  sorry
+
+-- @[equational_result]
+theorem Equation1491_facts : ∃ (G : Type) (_ : Magma G), Facts G [1491] [65] := by
+  use A, ⟨initial.closure⟩
+  simp only [Equation1491, closure_prop, implies_true, not_forall, true_and]
+  constructor
+  · exact closure_prop initial
+  · sorry
