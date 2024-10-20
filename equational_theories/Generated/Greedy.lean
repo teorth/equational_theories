3166cd87f8b71d38f18f5f07869130260d4182ea
Daniel Weber <55664973+Command-Master@users.noreply.github.com>
2024-10-20 17:31:53 +0300

    Add `Eq511` to `Greedy.lean` (#683)

--- equational_theories/Generated/Greedy.lean
+++ equational_theories/Generated/Greedy.lean
@@ -2,6 +2,7 @@ import equational_theories.Generated.Greedy.Eq118
 import equational_theories.Generated.Greedy.Eq124
 import equational_theories.Generated.Greedy.Eq476
 import equational_theories.Generated.Greedy.Eq503
+import equational_theories.Generated.Greedy.Eq511
 import equational_theories.Generated.Greedy.Eq677
 import equational_theories.Generated.Greedy.Eq707
 import equational_theories.Generated.Greedy.Eq883
