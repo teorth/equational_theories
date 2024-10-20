85775aef23bbc9304f84d040cc8e1992a4ca53a7
Daniel Weber <commandmaster2017@gmail.com>
2024-10-20 08:46:14 +0300

    Removed proven conjectures

--- equational_theories/Conjectures.lean
+++ equational_theories/Conjectures.lean
@@ -38,50 +38,6 @@ conjecture Equation1491_facts : ∃ (G : Type) (_ : Magma G), Facts G [1491] [65
 
 /-- See https://leanprover.zulipchat.com/#narrow/stream/458659-Equational/topic/1076.20!.3D.3E.203/near/477009119 -/
 @[equational_result]
-conjecture Equation118_facts : ∃ (G : Type) (_ : Magma G), Facts G [118] [222, 274, 4435]
-
-/-- See https://leanprover.zulipchat.com/#narrow/stream/458659-Equational/topic/1076.20!.3D.3E.203/near/477009119 -/
-@[equational_result]
-conjecture Equation124_facts : ∃ (G : Type) (_ : Magma G), Facts G [124] [1924]
-
-/-- See https://leanprover.zulipchat.com/#narrow/stream/458659-Equational/topic/1076.20!.3D.3E.203/near/477009119 -/
-@[equational_result]
-conjecture Equation476_facts : ∃ (G : Type) (_ : Magma G), Facts G [476] [359, 3862, 4065]
-
-/-- See https://leanprover.zulipchat.com/#narrow/stream/458659-Equational/topic/1076.20!.3D.3E.203/near/477009119 -/
-@[equational_result]
-conjecture Equation503_facts : ∃ (G : Type) (_ : Magma G), Facts G [503] [359, 3862, 4065]
-
-/-- See https://leanprover.zulipchat.com/#narrow/stream/458659-Equational/topic/1076.20!.3D.3E.203/near/477009119 -/
-@[equational_result]
-conjecture Equation677_facts : ∃ (G : Type) (_ : Magma G), Facts G [677] [255]
-
-/-- See https://leanprover.zulipchat.com/#narrow/stream/458659-Equational/topic/1076.20!.3D.3E.203/near/477009119 -/
-@[equational_result]
-conjecture Equation707_facts : ∃ (G : Type) (_ : Magma G), Facts G [707] [1223, 1316, 2238, 2847, 2940]
-
-/-- See https://leanprover.zulipchat.com/#narrow/stream/458659-Equational/topic/1076.20!.3D.3E.203/near/477009119 -/
-@[equational_result]
-conjecture Equation713_facts : ∃ (G : Type) (_ : Magma G), Facts G [713] [359, 817, 1426, 3862, 4065]
-
-/-- See https://leanprover.zulipchat.com/#narrow/stream/458659-Equational/topic/1076.20!.3D.3E.203/near/477009119 -/
-@[equational_result]
-conjecture Equation883_facts : ∃ (G : Type) (_ : Magma G), Facts G [883] [2035, 2101, 2238, 2304]
-
-/-- See https://leanprover.zulipchat.com/#narrow/stream/458659-Equational/topic/1076.20!.3D.3E.203/near/477009119 -/
-@[equational_result]
-conjecture Equation906_facts : ∃ (G : Type) (_ : Magma G), Facts G [906] [3862]
-
-/-- See https://leanprover.zulipchat.com/#narrow/stream/458659-Equational/topic/1076.20!.3D.3E.203/near/477009119 -/
-@[equational_result]
-conjecture Equation1112_facts : ∃ (G : Type) (_ : Magma G), Facts G [1112] [8, 411, 1629, 1832, 3253, 3319, 3862, 3915]
-
-/-- See https://leanprover.zulipchat.com/#narrow/stream/458659-Equational/topic/1076.20!.3D.3E.203/near/477009119 -/
-@[equational_result]
-conjecture Equation1113_facts : ∃ (G : Type) (_ : Magma G), Facts G [1113] [2441, 2534]
-
-/-- See https://leanprover.zulipchat.com/#narrow/stream/458659-Equational/topic/1076.20!.3D.3E.203/near/477009119 -/
-@[equational_result]
 conjecture Equation1289_facts : ∃ (G : Type) (_ : Magma G), Facts G [1289] [2507, 3116, 4435]
 
 /-- See https://leanprover.zulipchat.com/#narrow/stream/458659-Equational/topic/1076.20!.3D.3E.203/near/477009119 -/
@@ -91,14 +47,6 @@ conjecture Equation1447_facts : ∃ (G : Type) (_ : Magma G), Facts G [1447] [14
 /-- See https://leanprover.zulipchat.com/user_uploads/3121/ASjTo5huToAvNGcg7pOGOOSy/Equation1692.pdf -/
 @[equational_result]
 conjecture Equation1692_facts : ∃ (G : Type) (_ : Magma G), Facts G [1692] [23, 47, 1832, 2441, 3050, 3456, 4065]
-
-/-- See https://leanprover.zulipchat.com/#narrow/stream/458659-Equational/topic/1076.20!.3D.3E.203/near/477116580 -/
-@[equational_result]
-conjecture Equation1648_facts : ∃ (G : Type) (_ : Magma G), Facts G [1648] [3253]
-
-/-- See https://leanprover.zulipchat.com/#narrow/stream/458659-Equational/topic/1076.20!.3D.3E.203/near/477116580 -/
-@[equational_result]
-conjecture Equation511_facts : ∃ (G : Type) (_ : Magma G), Facts G [511] [614, 714, 1020, 1223, 2238, 2338, 2441, 2847, 3050, 4380, 4435]
 
 /-- https://leanprover.zulipchat.com/#narrow/channel/458659-Equational/topic/1841.20.3F.3D.3E.20203/near/477517055 -/
 @[equational_result]
