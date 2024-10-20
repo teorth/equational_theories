a0175642fda8b1e6432e87ca8c564a2326254778
Terence Tao <tao@math.ucla.edu>
2024-10-20 15:02:45 -0700

    add import for ManuallyProved

--- equational_theories/ManuallyProved.lean
+++ equational_theories/ManuallyProved.lean
@@ -2,4 +2,4 @@ import equational_theories.ManuallyProved.Equation1648
 import equational_theories.ManuallyProved.Equation1659
 import equational_theories.ManuallyProved.Equation1661
 import equational_theories.ManuallyProved.Equation1701
-
+import equational_theories.ManuallyProved.Equation3342
