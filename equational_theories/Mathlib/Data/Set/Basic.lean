8512f8270107007c5250cb2ac92ea9e016c38b67
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2024-10-20 17:16:57 +0200

    add pr link

--- equational_theories/Mathlib/Data/Set/Basic.lean
+++ equational_theories/Mathlib/Data/Set/Basic.lean
@@ -2,7 +2,7 @@ import Mathlib.Data.Set.Basic
 
 variable {α : Type*} {s : Set α} {t : Set α}
 
--- Mathlib PR:
+-- Mathlib PR: https://github.com/leanprover-community/mathlib4/pull/17973
 theorem Set.diff_insert_of_not_mem {x : α} (h : x ∉ s) : s \ insert x t = s \ t := by
   refine antisymm (diff_subset_diff (refl _) (subset_insert ..)) fun y hy ↦ ?_
   simp only [mem_diff, mem_insert_iff, not_or] at hy ⊢
