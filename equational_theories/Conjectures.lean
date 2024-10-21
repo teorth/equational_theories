cfad2deb7d3f9b663bf2e69922ac9590c3f6c63c
teorth <tao@math.ucla.edu>
2024-10-21 08:07:25 -0700

    add 1518 conjecture (#698)

--- equational_theories/Conjectures.lean
+++ equational_theories/Conjectures.lean
@@ -75,5 +75,8 @@ conjecture Equation1722_facts : ∃ (G : Type) (_ : Magma G), Facts G [1722] [26
 -/
 conjecture Equation3352_facts : ∃ (G : Type) (_ : Magma G), Facts G [3352] [4065]
 
+/-- https://leanprover.zulipchat.com/#narrow/channel/458659-Equational/topic/Outstanding.20equations.2C.20v1/near/478083079 -/
+conjecture Equation1518_facts : ∃ (G : Type) (_ : Magma G), Facts G [1518] [47, 614, 817, 3862]
+
 
 end Conjectures
