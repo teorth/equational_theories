400bb7f73bee28afdef922dcfd625e77d8a0c243
Daniel Weber <commandmaster2017@gmail.com>
2024-10-21 06:42:13 +0300

    Commit

---
+++ equational_theories/ManuallyProved/Equation1117.lean
@@ -0,0 +1,14 @@
+import equational_theories.EquationalResult
+import equational_theories.Equations.All
+
+def op (a b : ℤ) : ℤ := 2 * a - b / 2
+
+theorem Equation1117_not_implies_Equation2441 : ∃ (G : Type) (_ : Magma G), Equation1117 G ∧ ¬ Equation2441 G := by
+  use ℤ, ⟨op⟩
+  constructor
+  · intro x y z
+    simp [op]
+    omega
+  · simp only [not_forall, op]
+    use 1
+    decide
