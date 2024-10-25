7af88fde3e78236531da0a940c11b851c89fc99f
Daniel Weber <55664973+Command-Master@users.noreply.github.com>
2024-10-25 17:39:26 +0300

    Make "satisfies" in FME link to Graphiti (#732)
    null
    
    Closes #728

--- tools/fme/src/Main.elm
+++ tools/fme/src/Main.elm
@@ -2,7 +2,7 @@ port module Main exposing (..)
 
 import Browser
 import Browser.Navigation
-import Html exposing (Html, div, text, input, button, textarea, p, h1, h3, span, ul, li, pre, label)
+import Html exposing (Html, div, text, input, button, textarea, p, a, h1, h3, span, ul, li, pre, label)
 import Html.Attributes exposing (placeholder, value, id, class, rows)
 import Html.Events exposing (onClick, onInput)
 import Url exposing (Url, percentEncode, percentDecode)
@@ -11,6 +11,9 @@ import Maybe exposing (withDefault)
 import Set
 
 import Parsing exposing (parseInput)
+import Html.Attributes exposing (href)
+import Html exposing (Attribute)
+import Debug exposing (toString)
 
 
 {- MODEL -}
@@ -147,6 +150,13 @@ viewEquationTags tags =
   div [ class "scrollable-container" ]
     [ul [] (List.map viewEquationTag tags)]
 
+graphitiEq : (Int, String) -> String
+graphitiEq (a, _) = String.fromInt a ++ ","
+
+graphitiLink : List (Int, String) -> String
+graphitiLink tags = "/equational_theories/graphiti/?render=true&limit_equations=" ++
+  (String.join "+" (List.map graphitiEq tags))
+
 matchInput : String -> (Int, String) -> Bool
 matchInput matcher tag =
   case tag of
@@ -240,7 +250,7 @@ viewModelInfo table matcher mi =
         else List.filter (matchInput tmatcher) xs
   in
     div []
-      [ h3 [] [text "Satisfies: "]
+      [ h3 [] [a [ href (graphitiLink (matching mi.satisfied))] [ text "Satisfies"], text ": "]
       , viewEquationTags (matching mi.satisfied)
       , h3 [] [text "Refutes: "]
       , viewEquationTags (matching mi.refuted)
