99b84652be36eb01c5c62b8ece9a1f1bfcd5f240
Mario Carneiro <di.gama@gmail.com>
2024-10-26 04:23:34 +0200

    names

--- equational_theories.lean
+++ equational_theories.lean
@@ -15,6 +15,7 @@ import equational_theories.Preorder
 import equational_theories.SmallMagmas
 import equational_theories.StringMagmas
 import equational_theories.Homomorphisms
+import equational_theories.WeakCentralGroupoids
 import equational_theories.CentralGroupoids
 import equational_theories.Z3Counterexamples
 import equational_theories.LinearOps
