6e71b0272b76663b67ca867536f85a58a3f87900
Cody Roux <cody.roux@gmail.com>
2024-10-26 11:44:12 -0400

    Turn "Sheffer algebras" into a structure, to make for a cleaner statement.

--- equational_theories/BooleanMagma.lean
+++ equational_theories/BooleanMagma.lean
@@ -5,72 +5,72 @@ section ShafferLaws
 
 open BooleanRing
 
-variable {α : Type*}
-variable [Magma α]
-
-def prime (a : α) := a ◇ a
-
 
 -- Taking notations from https://www.cs.unm.edu/~mccune/papers/basax/v12.pdf and
 -- the original Sheffer paper, "A set of five independent postulates for Boolean algebras,
 -- with application to logical constants"
+class Stroke (α : Type*) where
+  stroke : α → α → α
+
+infix:80 " | " => Stroke.stroke
+
+def prime {α} [Stroke α] (a : α) := a | a
+
 postfix:max " ′ " => prime
 
--- These three laws axiomatize the Sheffer stroke, or NAND, entirely.
+-- The three laws axiomatize the Sheffer stroke, or NAND, entirely.
 -- The remainder of this file is dedicated to that proof.
-variable (sh₁ : ∀ a : α, a′′ = a)
-variable (sh₂ : ∀ a b : α, a ◇ (b ◇ b′) = a′)
-variable (sh₃ : ∀ a b c : α, (a ◇ (b ◇ c))′ = (b′ ◇ a) ◇ (c′ ◇ a))
-
+class Sheffer (α : Type*) extends Stroke α where
 -- We need to assume α is nonempty
-variable (e : α)
+elt : α
+sh₁ : ∀ a : α, a′′ = a
+sh₂ : ∀ a b : α, a | (b | b′) = a′
+sh₃ : ∀ a b c : α, (a | (b | c))′ = (b′ | a) | (c′ | a)
 
-include sh₁ sh₂ sh₃
+variable {α : Type*}
 
-def z : α := e ◇ e′
+variable [Sheffer α]
 
-def u : α := (z e)′
+open Sheffer
 
-@[simp]
-def sheffer₁ : ∀ a : α, a′′ = a := sh₁
+def z : α := elt | elt′
 
-def sheffer₂ : ∀ a b : α, a ◇ (b ◇ b′) = a′ := sh₂
+def u : α := z′
 
-def sheffer₃ : ∀ a b c : α, (a ◇ (b ◇ c))′ = (b′ ◇ a) ◇ (c′ ◇ a) := sh₃
-
-omit sh₂ in
-lemma commut (a b : α) : a ◇ b = b ◇ a :=
+lemma commut (a b : α) : a | b = b | a :=
 by
   calc
-    a ◇ b = (a ◇ b)′′ := by rw [sh₁] -- exact Eq.symm (sh₁ _)
-    _     = (a ◇ (b′′))′′ := by rw [sh₁ b]
-    _     = (b′′ ◇ a)′′ := by simp [prime] at * ; rw [sh₃ a (b◇b) (b◇b)]
-    _     = b ◇ a := by repeat rw [sh₁]
+    a | b = (a | b)′′ := by rw [sh₁] -- exact Eq.symm (sh₁ _)
+    _     = (a | (b′′))′′ := by rw [sh₁ b]
+    _     = (b′′ | a)′′ := by conv => left; arg 1; arg 1; simp [prime]
+                              conv => left; arg 1; rw [sh₃]
+                              exact rfl
+    _     = b | a := by repeat rw [sh₁]
 
-lemma all_bot (a b : α) : a ◇ a′ = b ◇ b′ :=
+lemma all_bot (a b : α) : a | a′ = b | b′ :=
 by
   calc
-    a ◇ a′ = (a ◇ a′)′′ := by rw [sh₁]
-    _      = ((a ◇ a′) ◇ (b ◇ b′))′ := by rw [sh₂]
-    _      = ((b ◇ b′) ◇ (a ◇ a′))′ := by rw [commut sh₁ sh₃]
-    _      = (b ◇ b′)′′ := by rw [sh₂]
-    _      = b ◇ b′     := by rw [sh₁]
+    a | a′ = (a | a′)′′ := by rw [sh₁]
+    _      = ((a | a′) | (b | b′))′ := by rw [sh₂]
+    _      = ((b | b′) | (a | a′))′ := by rw [commut]
+    _      = (b | b′)′′ := by rw [sh₂]
+    _      = b | b′     := by rw [sh₁]
 
 instance ShefferToBooleanRing : BooleanRing α where
-mul := (.′ ◇ .′)
-add := λ a b ↦ (a ◇ b)′
-zero := z e
-one := u e
+mul := (.′ | .′)
+add := λ a b ↦ (a | b)′
+zero := z
+one := u
 compl := (.′)
 commut₁ :=
 by
   intros a b; simp [HAdd.hAdd]
   apply congr; trivial
-  exact @commut _ _ sh₁ sh₃ a b
+  exact commut a b
 commut₂ :=
 by
   intros a b; simp [HMul.hMul]
-  exact @commut _ _ sh₁ sh₃ a ′ b ′
+  exact commut a ′ b ′
 ident₁ :=
 by
   intros a; simp [HAdd.hAdd, OfNat.ofNat]
@@ -85,8 +85,8 @@ by
   rw [sh₃]; repeat rw [sh₁]
   apply congr
   . apply congr; trivial
-    exact commut sh₁ sh₃ b a
-  . exact commut sh₁ sh₃ c a
+    exact commut b a
+  . exact commut c a
 distrib₂ :=
 by
   intros a b c; simp [HAdd.hAdd, HMul.hMul]
@@ -95,17 +95,17 @@ by
   rw [sh₃]
   apply congr; trivial; apply congr
   . apply congr; trivial
-    exact commut sh₁ sh₃ b ′ a ′
-  . exact commut sh₁ sh₃ c ′ a ′
+    exact commut b ′ a ′
+  . exact commut c ′ a ′
 compl₁ :=
 by
   intros a; simp [HAdd.hAdd, OfNat.ofNat, u, z]
   apply congr; trivial
-  exact all_bot sh₁ sh₂ sh₃ a e
+  exact all_bot a elt
 compl₂ :=
 by
   intros a; simp [HMul.hMul, OfNat.ofNat, u, z]
-  rw [sh₁, commut sh₁ sh₃]
-  exact all_bot sh₁ sh₂ sh₃ a e
+  rw [sh₁, commut]
+  exact all_bot a elt
 
 end ShafferLaws
