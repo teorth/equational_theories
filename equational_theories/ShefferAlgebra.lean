5f08ba5bdffc89c02fc16326252f5ccb905164eb
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2024-11-10 14:24:22 +0100

    golf and format `ShefferAlgebra .lean`

--- equational_theories/ShefferAlgebra.lean
+++ equational_theories/ShefferAlgebra.lean
@@ -30,18 +30,16 @@ variable [ShefferAlgebra α]
 
 open ShefferAlgebra
 
-lemma commut (a b : α) : a | b = b | a :=
-by
+lemma commut (a b : α) : a | b = b | a := by
   calc
     a | b = (a | b)′′ := by rw [sh₁]
     _     = (a | (b′′))′′ := by rw [sh₁ b]
     _     = (b′′ | a)′′ := by conv => left; arg 1; arg 1; simp [prime]
                               conv => left; arg 1; rw [sh₃]
-                              exact rfl
+                              rfl
     _     = b | a := by repeat rw [sh₁]
 
-lemma all_bot (a b : α) : a | a′ = b | b′ :=
-by
+lemma all_bot (a b : α) : a | a′ = b | b′ := by
   calc
     a | a′ = (a | a′)′′ := by rw [sh₁]
     _      = ((a | a′) | (b | b′))′ := by rw [sh₂]
@@ -68,19 +66,15 @@ def z : α := elt | elt′
 
 def u : α := z′
 
-lemma commut₁ (a b : α) : a ⊔ b = b ⊔ a := by
-  simp only [sup, commut]
+lemma commut₁ (a b : α) : a ⊔ b = b ⊔ a := by simp only [sup, commut]
 
-lemma commut₂ (a b : α) : a ⊓ b = b ⊓ a := by
-  simp only [inf, commut]
+lemma commut₂ (a b : α) : a ⊓ b = b ⊓ a := by simp only [inf, commut]
 
 @[simp]
-lemma ident₁ (a : α) : a ⊔ z = a := by
-  simp only [sup, z, sh₁, sh₂]
+lemma ident₁ (a : α) : a ⊔ z = a := by simp only [sup, z, sh₁, sh₂]
 
 @[simp]
-lemma ident₂ (a : α) : a ⊓ u = a := by
-  simp only [inf, u, z, sh₁, sh₂]
+lemma ident₂ (a : α) : a ⊓ u = a := by simp only [inf, u, z, sh₁, sh₂]
 
 lemma distrib₁ (a b c : α) : a ⊔ (b ⊓ c) = (a ⊔ b) ⊓ (a ⊔ c) := by
   simp only [sup, inf, sh₁, sh₃, commut]
@@ -89,20 +83,12 @@ lemma distrib₂ (a b c : α) : a ⊓ (b ⊔ c) = (a ⊓ b) ⊔ (a ⊓ c) := by
   simp only [sup, inf, sh₁]
   conv => left; exact (sh₁ _).symm
   rw [sh₃]
-  apply congr
-  trivial
-  apply congr
-  . apply congr
-    trivial
-    exact commut b′ a′
-  . exact commut c′ a′
+  exact congr rfl (congr (congr rfl (commut ..)) (commut ..))
 
 @[simp]
 lemma compl₁ (a : α) : a ⊔ aᶜ = u := by
   simp only [sup, u, z, comple]
-  apply congr
-  trivial
-  exact all_bot a elt
+  exact congr rfl (all_bot a elt)
 
 @[simp]
 lemma compl₂ (a  : α) : a ⊓ aᶜ = z := by
@@ -110,61 +96,55 @@ lemma compl₂ (a  : α) : a ⊓ aᶜ = z := by
   exact all_bot a elt
 
 @[simp]
-lemma bound₁ (a : α) : a ⊔ u = u :=
-by
+lemma bound₁ (a : α) : a ⊔ u = u := by
   calc
     a ⊔ u = (a ⊔ u) ⊓ u        := by rw [ident₂]
     _     = u ⊓ (a ⊔ u)        := by rw [commut₂]
     _     = (a ⊔ aᶜ) ⊓ (a ⊔ u) := by rw [compl₁]
     _     = a ⊔ (aᶜ ⊓ u)       := by rw [distrib₁]
     _     = a ⊔ aᶜ             := by rw [ident₂]
-    _     = u                  := by exact compl₁ a
+    _     = u                  := compl₁ a
 
 @[simp]
-lemma bound₂ (a : α) : a ⊓ z = z :=
-by
+lemma bound₂ (a : α) : a ⊓ z = z := by
   calc
     a ⊓ z = (a ⊓ z) ⊔ z        := by rw [ident₁]
     _     = z ⊔ (a ⊓ z)        := by rw [commut₁]
     _     = (a ⊓ aᶜ) ⊔ (a ⊓ z) := by rw [compl₂]
     _     = a ⊓ (aᶜ ⊔ z)       := by rw [distrib₂]
     _     = a ⊓ aᶜ             := by rw [ident₁]
-    _     = z                  := by exact compl₂ a
+    _     = z                  := compl₂ a
 
 @[simp] -- This simp is a little overeager.
-lemma absorb₁ (a b : α) : a ⊔ (a ⊓ b) = a :=
-by
+lemma absorb₁ (a b : α) : a ⊔ (a ⊓ b) = a := by
   calc
     a ⊔ (a ⊓ b) = (a ⊓ u) ⊔ (a ⊓ b) := by rw [ident₂]
     _           = a ⊓ (u ⊔ b)       := by rw [distrib₂]
     _           = a ⊓ (b ⊔ u)       := by conv => lhs; rw [commut₁]
     _           = a ⊓ u             := by rw [bound₁]
-    _           = a                 := by exact ident₂ a
+    _           = a                 := ident₂ a
 
 -- it would be nice to have a "dualization" tactic. This might be some work though.
 @[simp] -- This simp is a little overeager.
-lemma absorb₂ (a b : α) : a ⊓ (a ⊔ b) = a :=
-by
+lemma absorb₂ (a b : α) : a ⊓ (a ⊔ b) = a := by
   calc
     a ⊓ (a ⊔ b) = (a ⊔ z) ⊓ (a ⊔ b) := by rw [ident₁]
     _           = a ⊔ (z ⊓ b)       := by rw [distrib₁]
     _           = a ⊔ (b ⊓ z)       := by conv => lhs; rw [commut₂]
     _           = a ⊔ z             := by rw [bound₂]
-    _           = a                 := by exact ident₁ a
+    _           = a                 := ident₁ a
 
 @[simp]
-lemma idemp₂ (a : α) : a ⊓ a = a :=
-by
+lemma idemp₂ (a : α) : a ⊓ a = a := by
   symm
   calc
-    a = a ⊓ (a ⊔ z) := by exact Eq.symm (absorb₂ a z)
+    a = a ⊓ (a ⊔ z) := Eq.symm (absorb₂ a z)
     _ = a ⊓ a       := by rw [ident₁]
 
-lemma inv (a a' : α) : a ⊔ a' = u → a ⊓ a' = z → a' = aᶜ :=
-by
-  intros h₁ h₂
+lemma inv (a a' : α) : a ⊔ a' = u → a ⊓ a' = z → a' = aᶜ := by
+  intro h₁ h₂
   calc
-    a' = a' ⊓ u               := by exact Eq.symm (ident₂ a')
+    a' = a' ⊓ u               := Eq.symm (ident₂ a')
     _  = a' ⊓ (a ⊔ aᶜ)        := by rw [compl₁]
     _  = (a' ⊓ a) ⊔ (a' ⊓ aᶜ) := by rw [distrib₂]
     _  = (a' ⊓ a) ⊔ (aᶜ ⊓ a') := by conv => left; right; exact commut₂ a' aᶜ
@@ -174,59 +154,54 @@ by
     _  = (aᶜ ⊓ a) ⊔ (aᶜ ⊓ a') := by conv => left; left; exact commut₂ a aᶜ
     _  = aᶜ ⊓ (a ⊔ a')        := by rw [distrib₂]
     _  = aᶜ ⊓ u               := by rw [h₁]
-    _  = aᶜ                   := by exact ident₂ aᶜ
+    _  = aᶜ                   := ident₂ aᶜ
 
-lemma dne (a : α) : aᶜᶜ = a :=
-by
+lemma dne (a : α) : aᶜᶜ = a := by
   symm
   apply inv
   . rw [commut₁]; exact compl₁ a
   . rw [commut₂]; exact compl₂ a
 
-lemma inv_elim (a b : α) : aᶜ = bᶜ → a = b :=
-by
-  intros h
-  have h' : aᶜᶜ = bᶜᶜ := by congr
-  simp [dne] at h'; trivial
+lemma inv_elim (a b : α) : aᶜ = bᶜ → a = b := by
+  intro h
+  have h' : aᶜᶜ = bᶜᶜ := congrArg compl h
+  simp only [dne] at h'
+  trivial
 
-lemma cancel (a b : α) : a ⊔ bᶜ = u → a ⊓ bᶜ = z → a = b :=
-by
-  intros h₁ h₂
+lemma cancel (a b : α) : a ⊔ bᶜ = u → a ⊓ bᶜ = z → a = b := by
+  intro h₁ h₂
   have h : bᶜ = aᶜ := by apply inv <;> trivial
   apply inv_elim; symm; trivial
 
 @[simp]
-lemma A₁ (a b : α) : a ⊔ (aᶜ ⊔ b) = u :=
-by
+lemma A₁ (a b : α) : a ⊔ (aᶜ ⊔ b) = u := by
   calc
     a ⊔ (aᶜ ⊔ b) = (a ⊔ (aᶜ ⊔ b)) ⊓ u := by rw [ident₂]
     _            = u ⊓ (a ⊔ (aᶜ ⊔ b)) := by rw [commut₂]
-    _            = (a ⊔ aᶜ) ⊓ (a ⊔ (aᶜ ⊔ b)) := by simp
+    _            = (a ⊔ aᶜ) ⊓ (a ⊔ (aᶜ ⊔ b)) := by rw [compl₁]
     _            = a ⊔ (aᶜ ⊓ (aᶜ ⊔ b)) := by rw [distrib₁]
-    _            = a ⊔ aᶜ := by simp
-    _            = u := by simp
+    _            = a ⊔ aᶜ := by rw [absorb₂, compl₁]
+    _            = u := by rw [compl₁]
 
 @[simp]
-lemma A₂ (a b : α) : a ⊓ (aᶜ ⊓ b) = z :=
-by
+lemma A₂ (a b : α) : a ⊓ (aᶜ ⊓ b) = z := by
   calc
     a ⊓ (aᶜ ⊓ b) = (a ⊓ (aᶜ ⊓ b)) ⊔ z := by rw [ident₁]
     _            = z ⊔ (a ⊓ (aᶜ ⊓ b)) := by rw [commut₁]
-    _            = (a ⊓ aᶜ) ⊔ (a ⊓ (aᶜ ⊓ b)) := by simp
+    _            = (a ⊓ aᶜ) ⊔ (a ⊓ (aᶜ ⊓ b)) := by rw [compl₂]
     _            = a ⊓ (aᶜ ⊔ (aᶜ ⊓ b)) := by rw [distrib₂]
-    _            = a ⊓ aᶜ := by simp
-    _            = z := by simp
+    _            = a ⊓ aᶜ := by rw [absorb₁, compl₂]
+    _            = z := by rw [compl₂]
 
 
 @[simp]
-lemma dm₁ (a b : α) : (a ⊔ b)ᶜ = aᶜ ⊓ bᶜ :=
-by
+lemma dm₁ (a b : α) : (a ⊔ b)ᶜ = aᶜ ⊓ bᶜ := by
   symm
   apply cancel <;> simp [dne]
   . rw [commut₁, distrib₁]
     conv => left; left; rw [commut₁]; right; left; exact Eq.symm (dne a)
-    rw [A₁, commut₂, ident₂]
-    rw [commut₁]; conv => left; right; rw [commut₁]; left; exact Eq.symm (dne b)
+    rw [A₁, commut₂, ident₂, commut₁]
+    conv => left; right; rw [commut₁]; left; exact Eq.symm (dne b)
     simp
   . rw [distrib₂]
     conv => left; left; rw [commut₂]
@@ -235,8 +210,7 @@ by
     simp
 
 @[simp]
-lemma dm₂ (a b : α) : (a ⊓ b)ᶜ = aᶜ ⊔ bᶜ :=
-by
+lemma dm₂ (a b : α) : (a ⊓ b)ᶜ = aᶜ ⊔ bᶜ := by
   symm
   apply cancel <;> simp [dne]
   . rw [commut₂, distrib₁]
@@ -250,24 +224,16 @@ by
     rw [commut₂]; conv => left; right; rw [commut₂]; left; exact Eq.symm (dne b)
     rw [A₂]
 
-lemma D₁ (a b c : α) : (a ⊔ (b ⊔ c)) ⊔ aᶜ = u :=
-by
+lemma D₁ (a b c : α) : (a ⊔ (b ⊔ c)) ⊔ aᶜ = u := by
   rw [commut₁]
   conv => left; right; left; exact Eq.symm (dne a)
   simp
 
-lemma E₁ (a b c : α) : b ⊓ (a ⊔ (b ⊔ c)) = b :=
-by
-  rw [distrib₂]
-  simp; rw [commut₁]; simp
+lemma E₁ (a b c : α) : b ⊓ (a ⊔ (b ⊔ c)) = b := by rw [distrib₂, absorb₂, commut₁, absorb₁]
 
-lemma E₂ (a b c : α) : b ⊔ (a ⊓ (b ⊓ c)) = b :=
-by
-  rw [distrib₁]
-  simp; rw [commut₂]; simp
+lemma E₂ (a b c : α) : b ⊔ (a ⊓ (b ⊓ c)) = b := by rw [distrib₁, absorb₁, commut₂, absorb₂]
 
-lemma F₁ (a b c : α) : (a ⊔ (b ⊔ c)) ⊔ bᶜ = u :=
-by
+lemma F₁ (a b c : α) : (a ⊔ (b ⊔ c)) ⊔ bᶜ = u := by
   calc
     (a ⊔ (b ⊔ c)) ⊔ bᶜ = bᶜ ⊔ (a ⊔ (b ⊔ c)) := by rw [commut₁]
     _                  = u ⊓ (bᶜ ⊔ (a ⊔ (b ⊔ c))) := by rw [commut₂]; simp
@@ -277,30 +243,26 @@ by
     _                  = bᶜ ⊔ b := by rw [E₁]
     _                  = u := by rw [commut₁]; simp
 
-lemma G₁ (a b c : α) : (a ⊔ (b ⊔ c)) ⊔ cᶜ = u :=
-by
+lemma G₁ (a b c : α) : (a ⊔ (b ⊔ c)) ⊔ cᶜ = u := by
   conv => left; left; right; rw [commut₁]
   apply F₁
 
-lemma H₁ (a b c : α) : (a ⊔ b ⊔ c)ᶜ ⊓ a = z :=
-by
+lemma H₁ (a b c : α) : (a ⊔ b ⊔ c)ᶜ ⊓ a = z := by
   simp; rw [commut₂]
   calc
-    a ⊓ (aᶜ ⊓ bᶜ ⊓ cᶜ) = (a ⊓ (aᶜ ⊓ bᶜ ⊓ cᶜ)) ⊔ z := by simp
+    a ⊓ (aᶜ ⊓ bᶜ ⊓ cᶜ) = (a ⊓ (aᶜ ⊓ bᶜ ⊓ cᶜ)) ⊔ z := by rw [ident₁]
     _                  = z ⊔ (a ⊓ (aᶜ ⊓ bᶜ ⊓ cᶜ)) := by rw [commut₁]
-    _                  = (a ⊓ aᶜ) ⊔ (a ⊓ (aᶜ ⊓ bᶜ ⊓ cᶜ)) := by simp
+    _                  = (a ⊓ aᶜ) ⊔ (a ⊓ (aᶜ ⊓ bᶜ ⊓ cᶜ)) := by rw [compl₂]
     _                  = a ⊓ (aᶜ ⊔ (aᶜ ⊓ bᶜ ⊓ cᶜ)) := by rw [distrib₂]
     _                  = a ⊓ (aᶜ ⊔ (cᶜ ⊓ (aᶜ ⊓ bᶜ))) := by conv => left; right; right; rw [commut₂]
     _                  = a ⊓ aᶜ := by rw [E₂]
-    _                  = z := by simp
+    _                  = z := by rw [compl₂]
 
-lemma I₁ (a b c : α) : (a ⊔ b ⊔ c)ᶜ ⊓ b = z :=
-by
+lemma I₁ (a b c : α) : (a ⊔ b ⊔ c)ᶜ ⊓ b = z := by
   conv => left; left; arg 1; left; rw [commut₁]
-  apply H₁
+  exact H₁ ..
 
-lemma J₁ (a b c : α) : (a ⊔ b ⊔ c)ᶜ ⊓ c = z :=
-by
+lemma J₁ (a b c : α) : (a ⊔ b ⊔ c)ᶜ ⊓ c = z := by
   simp
   rw [commut₂]
   conv => left; right; rw [commut₂]
@@ -308,8 +270,7 @@ by
 
 -- Incredibly, these are derivable
 @[simp]
-lemma assoc₁ (a b c : α) : a ⊔ (b ⊔ c) = (a ⊔ b) ⊔ c :=
-by
+lemma assoc₁ (a b c : α) : a ⊔ (b ⊔ c) = (a ⊔ b) ⊔ c := by
   apply cancel; simp
   . calc
       (a ⊔ (b ⊔ c)) ⊔ (aᶜ ⊓ bᶜ ⊓ cᶜ) = ((a ⊔ (b ⊔ c)) ⊔ (aᶜ ⊓ bᶜ)) ⊓ ((a ⊔ (b ⊔ c)) ⊔ cᶜ) := by rw [distrib₁]
@@ -324,88 +285,79 @@ by
 
 -- We don't try to dualize the proof here, that's too painful, we apply de Morgan liberally
 @[simp]
-lemma assoc₂ (a b c : α) : a ⊓ (b ⊓ c) = (a ⊓ b) ⊓ c :=
-by
-  apply inv_elim; simp
+lemma assoc₂ (a b c : α) : a ⊓ (b ⊓ c) = (a ⊓ b) ⊓ c := by
+  apply inv_elim
+  simp
 
-instance ShefferLE : LE α :=
-  ⟨ λ a b ↦ a = b ⊓ a ⟩
+instance ShefferLE : LE α := ⟨ λ a b ↦ a = b ⊓ a ⟩
 
-lemma Sheffer.le_refl (a : α) : a ≤ a :=
-by
-  simp [ShefferLE]
+lemma Sheffer.le_refl (a : α) : a ≤ a := by simp [ShefferLE]
 
-lemma Sheffer.le_trans (a b c : α) :
-  a ≤ b → b ≤ c → a ≤ c :=
-by
-  simp [ShefferLE]
-  intros h₁ h₂
+lemma Sheffer.le_trans (a b c : α) : a ≤ b → b ≤ c → a ≤ c := by
+  rw [ShefferLE]
+  intro h₁ h₂
   calc
-    a = b ⊓ a       := by trivial
+    a = b ⊓ a       := h₁
     _ = (c ⊓ b) ⊓ a := by conv => lhs; rw [h₂]
-    _ = c ⊓ (b ⊓ a) := by exact Eq.symm (assoc₂ c b a)
+    _ = c ⊓ (b ⊓ a) := Eq.symm (assoc₂ c b a)
     _ = c ⊓ a       := by rw [← h₁]
 
-lemma Sheffer.le_antisymm (a b : α) :
-  a ≤ b → b ≤ a → a = b :=
-by
+lemma Sheffer.le_antisymm (a b : α) : a ≤ b → b ≤ a → a = b := by
   simp [ShefferLE]
-  intros h₁ h₂
+  intro h₁ h₂
   calc
-    a = b ⊓ a := by exact h₁
-    _ = a ⊓ b := by exact commut₂ b a
-    _ = b     := by exact id (Eq.symm h₂)
+    a = b ⊓ a := h₁
+    _ = a ⊓ b := commut₂ ..
+    _ = b     := id (Eq.symm h₂)
 
 
 instance ShefferToBooleanAlg : BooleanAlgebra α where
-sup := (. ⊔ .)
-le_refl := by exact fun a ↦ Sheffer.le_refl a
-le_trans := by exact fun a b c a_1 a_2 ↦ Sheffer.le_trans a b c a_1 a_2
-le_antisymm := by apply Sheffer.le_antisymm
-le_sup_left := by
-  intros a b
-  simp [Sup.sup, ShefferLE]
-  rw [commut₂]; exact Eq.symm (absorb₂ a b)
-le_sup_right := by
-  intros a b
-  simp [ShefferLE]
-  rw [commut₂, commut₁]; exact Eq.symm (absorb₂ b a)
-sup_le := by
-  intros a b c
-  simp [ShefferLE]
-  intros h₁ h₂
-  calc
-    a ⊔ b = (c ⊓ a) ⊔ b       := by conv => left; left; rw [h₁]
-    _     = (c ⊓ a) ⊔ (c ⊓ b) := by conv => left; right; rw [h₂]
-    _     = c ⊓ (a ⊔ b)       := by exact Eq.symm (distrib₂ c a b)
-inf := (. ⊓ .)
-inf_le_left := by
-  intros a b; simp [ShefferLE]
-inf_le_right := by
-  intros a b; simp only [ShefferLE]
-  calc
-    a ⊓ b = a ⊓ (b ⊓ b) := by simp
-    _     = a ⊓ b ⊓ b   := by exact assoc₂ a b b
-    _     = b ⊓ a ⊓ b   := by conv => left; left; rw [commut₂]
-    _     = b ⊓ (a ⊓ b) := by exact Eq.symm (assoc₂ b a b)
-le_inf := by
-  intros a b c; simp [ShefferLE]
-  intros h₁ h₂
-  calc
-    a = b ⊓ a       := by exact h₁
-    _ = b ⊓ (c ⊓ a) := by conv => left; right; rw [h₂]
-    _ = b ⊓ c ⊓ a  := by exact assoc₂ b c a
-le_sup_inf := by
-  intros a b c; simp 
-  rw [distrib₁]
-  exact Sheffer.le_refl ((a ⊔ b) ⊓ (a ⊔ c))
-top := u
-bot := z
-inf_compl_le_bot := by intros a; simp; apply Sheffer.le_refl
-top_le_sup_compl := by intros a; simp; apply Sheffer.le_refl
-le_top := by intros a; simp [ShefferLE]; rw [commut₂]; exact Eq.symm (ident₂ a)
-bot_le := by
-  intros a; simp [ShefferLE]
+  sup := (. ⊔ .)
+  le_refl := fun a ↦ Sheffer.le_refl a
+  le_trans := fun a b c a_1 a_2 ↦ Sheffer.le_trans a b c a_1 a_2
+  le_antisymm := Sheffer.le_antisymm
+  le_sup_left := by
+    intro a b
+    simp only [ShefferLE, Sup.sup]
+    rw [commut₂]
+    exact Eq.symm (absorb₂ a b)
+  le_sup_right := by
+    intro a b
+    simp only [ShefferLE]
+    rw [commut₂, commut₁]
+    exact Eq.symm (absorb₂ b a)
+  sup_le := by
+    intro a b c
+    simp only [ShefferLE]
+    intro h₁ h₂
+    calc
+      a ⊔ b = (c ⊓ a) ⊔ b       := by conv => left; left; rw [h₁]
+      _     = (c ⊓ a) ⊔ (c ⊓ b) := by conv => left; right; rw [h₂]
+      _     = c ⊓ (a ⊔ b)       := Eq.symm (distrib₂ ..)
+  inf := (. ⊓ .)
+  inf_le_left := by
+    intro a b; simp [ShefferLE]
+  inf_le_right := by
+    intro a b; simp only [ShefferLE]
+    calc
+      a ⊓ b = a ⊓ (b ⊓ b) := by rw [idemp₂]
+      _     = a ⊓ b ⊓ b   := assoc₂ a b b
+      _     = b ⊓ a ⊓ b   := by conv => left; left; rw [commut₂]
+      _     = b ⊓ (a ⊓ b) := Eq.symm (assoc₂ ..)
+  le_inf := by
+    intro a b c h₁ h₂
+    simp only [ShefferLE]
+    calc
+      a = b ⊓ a       := h₁
+      _ = b ⊓ (c ⊓ a) := by conv => left; right; rw [h₂]
+      _ = b ⊓ c ⊓ a  := by exact assoc₂ b c a
+  le_sup_inf := by intro a b c; simp; rw [distrib₁]; exact Sheffer.le_refl ..
+  top := u
+  bot := z
+  inf_compl_le_bot := by intro a; simp only [compl₂]; exact Sheffer.le_refl ..
+  top_le_sup_compl := by intro a; simp only [compl₁]; exact Sheffer.le_refl ..
+  le_top := by intro a; simp only [ShefferLE]; rw [commut₂]; exact Eq.symm (ident₂ a)
+  bot_le := by intro a; simp [ShefferLE]
 
 end ShefferLaws
 
@@ -417,12 +369,12 @@ variable [BooleanAlgebra α]
 -- This is intentionally not an instance to avoid creating an instance cycle
 -- Boolean Algebra -> Sheffer Algebra -> Boolean Algebra.
 def BooleanAlgToSheffer : ShefferAlgebra α where
-stroke x y := (x ⊓ y)ᶜ
-elt := ⊥
-sh₁ := by intros a; simp [prime]
-sh₂ := by intros a b; simp [prime]
-sh₃ := by intros a b c; simp [prime]; rw [inf_sup_left]
-          conv => left; left; rw [inf_comm]
-          conv => left; right; rw [inf_comm]
+  stroke x y := (x ⊓ y)ᶜ
+  elt := ⊥
+  sh₁ := by intro a; simp [prime]
+  sh₂ := by intro a b; simp [prime]
+  sh₃ := by intro a b c; simp [prime]; rw [inf_sup_left]
+            conv => left; left; rw [inf_comm]
+            conv => left; right; rw [inf_comm]
 
 end BooleanToSheffer
