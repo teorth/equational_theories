736de434c3c571154e619fb2b3c1ca202b42b3b3
Amir Livne Bar-on <amir.livne.baron@gmail.com>
2024-11-14 10:37:05 +0200

    fix the greedy construction order and start formalizing the proof

--- equational_theories/ManuallyProved/Equation879.lean
+++ equational_theories/ManuallyProved/Equation879.lean
@@ -3,6 +3,9 @@ import equational_theories.Equations.All
 import equational_theories.Mathlib.Order.Greedy
 import Mathlib.GroupTheory.FreeGroup.Basic
 
+-- Formalization of
+--    https://leanprover.zulipchat.com/user_uploads/3121/-Y2DILKZP-OuhxcU0HS-EHge/Equation879.pdf
+
 instance (α : Type*) [DecidableEq α] [Encodable α] : Encodable (FreeGroup α) where
   encode x := Encodable.encode x.toWord
   decode n := Encodable.decode n |>.map FreeGroup.mk
@@ -45,23 +48,55 @@ instance : Preorder PartialSolution where
   le_refl := by simp
   le_trans := by simp_all
 
+theorem le_x₁ (sol sol' : PartialSolution) (h : sol ≤ sol') : sol.x₁ = sol'.x₁ :=
+  sol'.isFun _ _ _ (h _ _ sol.atOne) sol'.atOne
+
 def PartialSolution.dom (sol : PartialSolution) : Set G :=
   { x | ∃ y, sol.E x y }
 
--- The heart of the proof, Lemma 1.1 in
---    https://leanprover.zulipchat.com/user_uploads/3121/-Y2DILKZP-OuhxcU0HS-EHge/Equation879.pdf
-def extend (sol : PartialSolution) (x : G) : { sol' // sol ≤ sol' ∧ x ∈ sol'.dom } :=
+def PartialSolution.satisfies_func_eq879 (sol : PartialSolution) (x : G) :=
+  ∃ y z, sol.E x y ∧ sol.E (sol.x₁ * y⁻¹) z -- ∧ sol.E (z * y * x⁻¹) x⁻¹
+
+theorem satisfies_func_eq879_dom (sol : PartialSolution) (x : G) (h : sol.satisfies_func_eq879 x) : x ∈ sol.dom := by
+  choose y _ _ _ using h
+  use y
+
+def extend_case1 (sol : PartialSolution) (a : G) (h : a ∈ sol.dom) :
+    { sol' // sol ≤ sol' ∧ sol'.satisfies_func_eq879 a ∧ ∃ b, sol'.E a b ∧ (sol'.x₁ * b⁻¹) ∈ sol'.dom } := by
   sorry
+
+def extend (sol : PartialSolution) (a : G) : { sol' // sol ≤ sol' ∧ sol'.satisfies_func_eq879 a } := by
+  by_cases h : a ∈ sol.dom
+  · have ⟨sol', _, _, _⟩ := extend_case1 sol a h
+    use sol'
+  · by_cases h2 : ∃ z, sol.E z (a⁻¹ * sol.x₁)
+    · choose z hz using h2
+      have ⟨sol', hle, _, hdom⟩ := extend_case1 sol z ⟨_, hz⟩
+      have ⟨sol'', hle', _, _⟩ := extend_case1 sol' a <| by
+        choose b hzb hbdom using hdom
+        rw [le_x₁ _ _ hle] at hz
+        apply hle at hz
+        have ha : a = sol'.x₁ * b⁻¹ := by
+          simp [congrArg (a * · * b⁻¹),
+                sol'.isFun z b _ hzb hz]
+        simp [hbdom, ha]
+      have : sol ≤ sol'' := le_trans hle hle'
+      use sol''
+    · sorry
 
 theorem exists_full_solution (seed : PartialSolution) :
     ∃ f : G → G, func_eq879 f ∧ ∀ a b, seed.E a b → f a = b := by
   have ⟨c, hc, _, _, h3⟩ := exists_greedy_chain
-    (fun x => { sol : PartialSolution | x ∈ sol.dom })
-    (fun sol x => let ⟨sol', h⟩ := extend sol x; ⟨sol', by simp [h]⟩)
+    (fun a => { sol : PartialSolution | sol.satisfies_func_eq879 a })
+    (fun sol a => let ⟨sol', h⟩ := extend sol a; ⟨sol', by simp [h]⟩)
     seed
 
   let E a b := ∃ sol ∈ c, sol.E a b
-  have hdom : ∀ x, ∃ y, E x y := by intro x; choose sol _ y _ using h3 x; use y, sol
+  have hdom : ∀ a, ∃ b, E a b := by
+    intro a
+    choose sol _ b heq using h3 a
+    have : sol.E a b := heq.choose_spec.1
+    use b, sol
   have isFun : ∀ x y z, E x y → E x z → y = z := by
     intro x y z ⟨sxy, hsxy, hxy⟩ ⟨sxz, hsxz, hxz⟩
     have ⟨s, sy, sz⟩ : ∃ s, sxy ≤ s ∧ sxz ≤ s := by
