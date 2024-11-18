779c98fcffde67f67ba7f9475da30a63f7335e74
b-reinke <b-reinke@users.noreply.github.com>
2024-11-18 23:24:55 +0100

    Greedy 917 (#853)
    This shows 917 !=> 1629 and 917 !=> 2441 via the translation invariant
    construction.
    
    I haven't shown 917 !=> 1729 or 917 !=> 2541, this was not in pdf. But
    we have 1729 => 1629 and 2541 => 2441. So I am removing all outgoing
    conjectures from 917.

--- equational_theories/ManuallyProved.lean
+++ equational_theories/ManuallyProved.lean
@@ -1,4 +1,5 @@
 import equational_theories.ManuallyProved.Equation854
+import equational_theories.ManuallyProved.Equation917
 import equational_theories.ManuallyProved.Equation1117
 import equational_theories.ManuallyProved.Equation1437
 import equational_theories.ManuallyProved.Equation1516
