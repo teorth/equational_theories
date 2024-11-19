163cd63556d8be0e28583b0c152b9df3918a07d6
Bernhard Reinke <b-reinke@users.noreply.github.com>
2024-11-19 12:09:23 +0100

    Translation invariant counterexamples for 1722

--- equational_theories/ManuallyProved.lean
+++ equational_theories/ManuallyProved.lean
@@ -9,4 +9,5 @@ import equational_theories.ManuallyProved.Equation1648
 import equational_theories.ManuallyProved.Equation1659
 import equational_theories.ManuallyProved.Equation1661
 import equational_theories.ManuallyProved.Equation1701
+import equational_theories.ManuallyProved.Equation1722
 import equational_theories.ManuallyProved.Equation3342
