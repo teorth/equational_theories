4dedd2681cfbf46cacb5da338bcc4b9c4a5a9ba9
Vlad Tsyrklevich <vlad@tsyrklevich.net>
2024-12-04 18:01:07 +0100

    Import minimized refutations for 476/503 (#979)
    I am adding Matthew Bolan's minimized examples from
    https://leanprover.zulipchat.com/#narrow/channel/458659-Equational/topic/Austin.20pairs/near/485534984

--- equational_theories/Generated/All4x4Tables.lean
+++ equational_theories/Generated/All4x4Tables.lean
@@ -933,3 +933,5 @@ import equational_theories.Generated.All4x4Tables.Refutation931
 import equational_theories.Generated.All4x4Tables.Refutation932
 import equational_theories.Generated.All4x4Tables.Refutation933
 import equational_theories.Generated.All4x4Tables.Refutation934
+import equational_theories.Generated.All4x4Tables.Refutation935
+import equational_theories.Generated.All4x4Tables.Refutation936
