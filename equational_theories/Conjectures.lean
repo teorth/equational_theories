f20faff740079f523befbe5ddc271792b55efbfc
teorth <tao@math.ucla.edu>
2024-12-09 09:23:15 -0800

    Update-stubs (#1000)

--- equational_theories/Conjectures.lean
+++ equational_theories/Conjectures.lean
@@ -3,7 +3,7 @@ import equational_theories.EquationalResult
 import equational_theories.Equations.All
 import Mathlib.Data.Set.Finite.Basic
 
-/- This file is for results currently conjectured from project discussion, but not yet formalized. -/
+/- This file is for results currently conjectured from project discussion, but not yet formalized, or assigned to specific files in `ManuallyProven.lean`. -/
 
 namespace Conjectures
 
@@ -28,25 +28,5 @@ conjecture Equation1729_facts : ∃ (G : Type) (_ : Magma G), Facts G [1729] [81
 
 /- Finite conjectures follow -/
 
-/-- https://leanprover.zulipchat.com/#narrow/channel/458659-Equational/topic/Austin.20pairs/near/481257624 -/
-@[equational_result]
-conjecture Finite.Equation467_implies_Equation2847 (G : Type) [Magma G] [Finite G] (_ : Equation467 G) : Equation2847 G
-@[equational_result]
-conjecture Finite.Equation1133_implies_Equation1167 (G : Type) [Magma G] [Finite G] (_ : Equation1133 G) : Equation1167 G
-@[equational_result]
-conjecture Finite.Equation1167_implies_Equation1096 (G : Type) [Magma G] [Finite G] (_ : Equation1167 G) : Equation1096 G
-@[equational_result]
-conjecture Finite.Equation1441_implies_Equation4067 (G : Type) [Magma G] [Finite G] (_ : Equation1441 G) : Equation4067 G
-@[equational_result]
-conjecture Finite.Equation1443_implies_Equation3055 (G : Type) [Magma G] [Finite G] (_ : Equation1443 G) : Equation3055 G
-@[equational_result]
-conjecture Finite.Equation1681_implies_Equation3877 (G : Type) [Magma G] [Finite G] (_ : Equation1681 G) : Equation3877 G
-@[equational_result]
-conjecture Finite.Equation1701_implies_Equation1035 (G : Type) [Magma G] [Finite G] (_ : Equation1701 G) : Equation1035 G
-
-/- The below is also reachable using the method of bijections from above -/
-/-- https://leanprover.zulipchat.com/#narrow/channel/458659-Equational/topic/Austin.20pairs/near/482525422 -/
-@[equational_result]
-conjecture Finite.Equation1516_implies_Equation255 (G : Type) [Magma G] [Finite G] (_ : Equation1516 G) : Equation255 G
 
 end Conjectures
--- equational_theories/ManuallyProved/Equation1441.lean
+++ equational_theories/ManuallyProved/Equation1441.lean
@@ -1,45 +1,34 @@
-import equational_theories.Mathlib.Data.List.Defs
-import equational_theories.Mathlib.Order.Greedy
-import Mathlib.Data.Finset.Order
-import Mathlib.Data.List.AList
-import Mathlib.Data.Set.Finite.Lattice
-import Mathlib.Data.Set.Basic
-import Mathlib.Data.Set.Functor
-import Mathlib.GroupTheory.FreeGroup.Basic
-
-import equational_theories.FreshGenerator
 import equational_theories.Equations.All
-import equational_theories.FactsSyntax
 
-
-/- When the theorems here are fully formalized, update `Conjectures.lean` and `ManuallyProved.lean` accordingly. -/
+/- Proves the implications clustered around the equation 1441.
+-/
 
 namespace Eq1441
 
 /- Proof can be found at https://teorth.github.io/equational_theories/blueprint/infinite-model-chapter.html#1441-4067-1443-3055 -/
 
 @[equational_result]
-theorem Finite.Equation1441_implies_Equation4067 (G : Type) [Magma G] [Finite G] (_ : Equation1441 G) : Equation4067 G := by sorry
+conjecture Finite.Equation1441_implies_Equation4067 (G : Type) [Magma G] [Finite G] (_ : Equation1441 G) : Equation4067 G
 
 
 
 /- Proof can be found at https://teorth.github.io/equational_theories/blueprint/infinite-model-chapter.html#1441-4067-1443-3055 -/
 
 @[equational_result]
-theorem Finite.Equation1443_implies_Equation3055 (G : Type) [Magma G] [Finite G] (_ : Equation1443 G) : Equation3055 G := by sorry
+conjecture Finite.Equation1443_implies_Equation3055 (G : Type) [Magma G] [Finite G] (_ : Equation1443 G) : Equation3055 G
 
 
 /- Proof can be found at https://teorth.github.io/equational_theories/blueprint/infinite-model-chapter.html#1681-3877-1701-1035 -/
 
 @[equational_result]
-theorem Finite.Equation1681_implies_Equation3877 (G : Type) [Magma G] [Finite G] (_ : Equation1681 G) : Equation3877 G := by sorry
+conjecture Finite.Equation1681_implies_Equation3877 (G : Type) [Magma G] [Finite G] (_ : Equation1681 G) : Equation3877 G
 
 
 
 /- Proof can be found at https://teorth.github.io/equational_theories/blueprint/infinite-model-chapter.html#1681-3877-1701-1035 -/
 
 @[equational_result]
-theorem Finite.Equation1701_implies_Equation1035 (G : Type) [Magma G] [Finite G] (_ : Equation1701 G) : Equation1035 G := by sorry
+conjecture Finite.Equation1701_implies_Equation1035 (G : Type) [Magma G] [Finite G] (_ : Equation1701 G) : Equation1035 G
 
 
 end Eq1441
