d4ea6daba509bd67531066306854430f849ea5e5
Lorenzo Luccioli <lorenzoluccioli@gmail.com>
2025-01-20 17:21:07 +0100

    partial proof of exist_extension and change statement

--- equational_theories/ManuallyProved/Equation1516.lean
+++ equational_theories/ManuallyProved/Equation1516.lean
@@ -1522,29 +1522,119 @@ variable [Extension]
 
 end Extension
 --maybe in  this part of the prof we can actually avoid using the greedy construction, at first glance it seems to me that we actually explicitely define the function at each
-theorem exists_extension :
+set_option pp.proofs true
+theorem exists_extension (seed : PartialSolution) :
     ∃ L : A → G → G,
-    (∀ a b : A, L b a = b ◇ a) ∧ -- Lb extends a : A ↦ b ◇ a
-    (∀ b : A, ∀ x : G', (L (S x) <| L b <| L b x) = b) ∧ -- Axiom B
-    (L 1 x₀ = .inl 1) ∧
-    (∀ b : A, ∀ x : G', {y : G' | L b y = x}.Infinite) -- infinite surjectivity
+      (∀ a b : A, L b a = b ◇ a) ∧ -- Lb extends a : A ↦ b ◇ a
+      (∀ b : A, ∀ x : G', (L (S x) <| L b <| L b x) = b) ∧ -- Axiom B
+      (L 1 x₀ = .inl 1) ∧
+      (∀ b : A, ∀ x : G', {y : G' | L b y = x}.Infinite) -- infinite surjectivity
     -- (∀ b : A, ∀ x : G', L b x ≠ x)
-    := by sorry
-    -- ∃ op : ExtBase G → ExtBase G → ExtBase G, ∃ E : ExtBase G → ExtBase G → Prop,
-    -- (∀ a b c, c = op a b ↔ IsGood a.1 c.1 b.1 ∧ E a c ∧ E c b) ∧
-    -- (∀ a b, (a, b) ∈ e₀.1 → E a b) ∧
-    -- (∀ a b, E a b → Path a.1 b.1) := by
-  -- have ⟨c, hc, h1, _, h3⟩ := exists_greedy_chain
-  --   (task := fun x : _ × _ => {e | (e.1.induced x.1 x.2).Nonempty}) (fun E ⟨a, b⟩ => E.next a b) e₀
-  -- simp only [Subtype.exists, Prod.forall] at h3
-  -- choose f hf1 hf2 op hop using h3
-  -- refine ⟨op, fun a b => ∃ e ∈ c, (a, b) ∈ e.1, ?_, fun a b H => ⟨_, h1, H⟩, ?_⟩
-  -- · refine fun a b c => ⟨fun H => ?_, fun ⟨h1, ⟨i, hi, h2⟩, ⟨j, hj, h3⟩⟩ => ?_⟩
-  --   · exact let ⟨h1, h2, h3⟩ := hop a b; H ▸ ⟨h1, ⟨_, hf2 _ _, h2⟩, ⟨_, hf2 _ _, h3⟩⟩
-  --   · have ⟨k, hk, ik, jk⟩ := hc.directedOn _ hi _ hj
-  --     have ⟨l, _, kl, fl⟩ := hc.directedOn _ hk _ (hf2 a b)
-  --     exact l.2.2 _ _ ⟨h1, le_trans ik kl h2, le_trans jk kl h3⟩ ((f ..).induced_mono fl (hop a b))
-  -- · exact fun a b ⟨i, _, hi⟩ => i.2.1 a b hi
+    := by
+  classical
+  have ⟨c, hc, h1, h2, h3⟩ := exists_greedy_chain (a := seed)
+    (task := fun (a, x) ↦ {e | ∃ y, e.1 a x y})
+    fun ⟨E, ok⟩ ((d, y) : A × G) ↦ by
+      if h : ∃ z, E d y z then exact ⟨_, le_rfl, h⟩
+      else
+        let E1 : Extension := { E, ok, d, y, not_def := fun h' ↦ h ⟨_, h'⟩ }
+        -- we need to define next here
+        -- exact ⟨E1.next, fun _ _ ↦ (.base ·), _, .new rfl rfl⟩
+        sorry
+  choose e he L hL using h3
+
+  -- have L_of_e {p : A × G} {a : A} {y x : G} (h : (e p).val a y x) : L (a, y) = x := by
+  --   rcases hc.total (he p) (he (a, y)) with (h_le | h_le)
+  --   · exact (e (a, y)).2.func (hL (a, y)) (h_le _ _ _ h)
+  --   · exact (e p).2.func (h_le _ _ _ (hL (a, y))) h
+
+  have L_of_e {a : A} {y x : G} {e₀ : PartialSolution} (he₀ : e₀   ∈ c)
+      (h : e₀.val a y x) : L (a, y) = x := by
+    rcases hc.total he₀ (he (a, y)) with (h_le | h_le)
+    · exact (e (a, y)).2.func (hL (a, y)) (h_le _ _ _ h)
+    · exact e₀.2.func (h_le _ _ _ (hL (a, y))) h
+
+  refine ⟨L.curry,
+    fun a b ↦ ?_,
+    fun a x ↦ ?_,
+    ?_,
+    -- fun b ⟨⟨a, c, n⟩, (hac : a ≠ c)⟩ ↦ ?_⟩
+    fun b ⟨x, hx⟩ ↦ ?_⟩
+
+  ·
+    simp
+    -- we may need to add something to the PartialSolution structure in order to make this work
+    sorry
+  · let T : Finset (A × G) := {
+      (a, (x : G)),
+      (a, (L (a, x))),
+      (S x, L (a, (L (a, x))))}
+    have ⟨⟨e, he⟩, le⟩ := hc.directed.finset_le (hι := ⟨⟨_, h1⟩⟩)
+      (T.image fun p ↦ ⟨e p, he p⟩)
+    have hT := fun p hp ↦ Finset.forall_image.mp le p hp _ _ _ (hL p)
+    simp only [Finset.mem_insert, Finset.mem_singleton, forall_eq_or_imp, forall_eq, T] at hT
+    have ⟨e_a_x, e_a_L, e_Sx_L⟩ := hT
+    exact L_of_e he <| e.2.aux1 e_a_x e_a_L
+  ·
+    -- we may need to add something to the PartialSolution structure in order to make this work
+    simp
+
+    sorry
+  ·
+    simp
+    rw [← Set.encard_eq_top_iff, ENat.eq_top_iff_forall_le]
+    intro n
+    set m := max n x.2.2
+    -- we have to find a PartialSolution e in the chain c such that n, x.2.2 ≤ {a | ∃ x y, ↑e a x y}.ncard, we do it by setting T' : Finset A that contains at least max(n, x.2.2) arbitrary elements of A, then we define T as {(a, x₀) | a ∈ T' ∪ {b, x.1, x.2.1}} and proceed as in the previous subproofs
+
+    have ⟨(T' : Finset A), hT'⟩ := Finset.exists_card_eq m
+    let T := Finset.image (fun a ↦ (a, x₀)) (T' ∪ {b, x.1, x.2.1})
+
+    have ⟨⟨e, he⟩, le⟩ := hc.directed.finset_le (hι := ⟨⟨_, h1⟩⟩)
+      (T.image fun p ↦ ⟨e p, he p⟩)
+    have hT := fun p hp ↦ Finset.forall_image.mp le p hp _ _ _ (hL p)
+
+    simp only [Finset.union_comm T', Finset.insert_union, ← Finset.insert_eq, Finset.image_insert,
+      Finset.mem_insert, Finset.mem_image, forall_eq_or_imp, forall_exists_index, and_imp,
+      forall_apply_eq_imp_iff₂, T] at hT
+
+    have ⟨e_b, e_x1, e_x2, e_T'⟩ := hT
+
+    have h := e.2.aux2 b ⟨x, hx⟩
+    simp_rw [← Finset.mem_coe, ← Set.ncard_coe_Finset, dom_projL_eq e.2.finite] at h
+
+    have h_finite : {a | ∃ x y, e.1 a x y}.Finite := by
+      -- have hinj : Function.Injective fun (a : A) ↦ (a, x₀, L (a, x₀)) := by
+      --   simp [Function.Injective]
+
+      -- apply Set.Finite.of_finite_image _ hinj.injOn
+      -- apply Set.Finite.subset e.2.finite
+      -- simp only [Set.image_subset_iff, Set.preimage_setOf_eq,
+      --   forall_exists_index]
+      -- intro x hx
+      -- dsimp
+      -- simp only [Set.mem_image, Set.mem_setOf_eq] at hx
+
+      --should be doable but a bit annoying, the above strategy is flawed because it chooses the wrong function to inject
+      sorry
+    have h_le : m ≤ {a | ∃ x y, e.1 a x y}.ncard := by
+      rw [← hT', ← Set.ncard_coe_Finset T']
+      refine Set.ncard_le_ncard ?_ h_finite
+      intro t ht
+      refine ⟨_, _, e_T' _ ht⟩
+
+    specialize h ⟨_, _, e_b⟩ ⟨_, _, e_x1⟩ ⟨_, _, e_x2⟩ <| (le_max_right _ _).trans h_le
+
+    refine (Set.ncard_le_encard {y : G'| L (b, y) = _}).trans' ?_
+    simp only [ENat.some_eq_coe, Nat.cast_le]
+
+    calc
+      _ ≤ m := le_max_left _ _
+      _ ≤ _ := h_le
+      _ ≤ _ := h
+
+    sorry
+
 
 noncomputable def L : A → G → G := exists_extension.choose
 
