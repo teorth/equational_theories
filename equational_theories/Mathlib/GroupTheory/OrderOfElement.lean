000d8f07961ab9a2884aff646db36068f0c088fe
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2025-05-13 06:10:33 +0200

    remove upstreamed files

--- equational_theories/Mathlib/GroupTheory/OrderOfElement.lean
+++ equational_theories/Mathlib/GroupTheory/OrderOfElement.lean
@@ -4,7 +4,8 @@ universe u
 
 variable {α : Type u}
 
-theorem isOfFinOrder_of_isConj [Group α] {x y : α} : IsConj x y → (IsOfFinOrder x → IsOfFinOrder y) := by
+theorem isOfFinOrder_of_isConj [Group α] {x y : α} :
+      IsConj x y → (IsOfFinOrder x → IsOfFinOrder y) := by
    simp only [isConj_iff]
    repeat rw [isOfFinOrder_iff_pow_eq_one]
    intro ⟨u, eq⟩ ⟨n, n_gt_0, eq'⟩
