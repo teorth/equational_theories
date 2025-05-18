ff176185efd5908c364d325b727aaea084dfa652
Bernhard Reinke <b-reinke@users.noreply.github.com>
2025-05-18 11:12:32 +0200

    mark upstreamed lemmas

--- equational_theories/Mathlib/Data/List/Lemmas.lean
+++ equational_theories/Mathlib/Data/List/Lemmas.lean
@@ -6,15 +6,18 @@ universe u
 
 variable {α : Type u}
 
-theorem append_cancel_right_length {as bs bs' cs : List α}
-(eq_length : bs.length = bs'.length) (h : as ++ bs = cs ++ bs') : (as = cs) := by
-  match as, cs with
-  | [], []       => rfl
-  | [], c::cs    => have aux := congrArg length h; simp +arith +decide [eq_length] at aux
-  | a::as, []    => have aux := congrArg length h; simp +arith +decide [eq_length] at aux
-  | a::as, c::cs => injection h with h₁ h₂; subst h₁; rw [append_cancel_right_length eq_length h₂]
+-- [DUPLICATE] PR: https://github.com/leanprover-community/mathlib4/pull/22511
+-- theorem append_cancel_right_length {as bs bs' cs : List α}
+-- (eq_length : bs.length = bs'.length) (h : as ++ bs = cs ++ bs') : (as = cs) := by
+--  match as, cs with
+--  | [], []       => rfl
+--  | [], c::cs    => have aux := congrArg length h; simp +arith +decide [eq_length] at aux
+--  | a::as, []    => have aux := congrArg length h; simp +arith +decide [eq_length] at aux
+--  | a::as, c::cs => injection h with h₁ h₂; subst h₁; rw [append_cancel_right_length eq_length h₂]
 
--- [UPSTREAMED]
+@[deprecated (since := "2025-05-18")] alias append_cancel_right_length := List.append_inj_left'
+
+-- [UPSTREAMED] PR: https://github.com/leanprover-community/mathlib4/pull/22510
 -- theorem head?_flatten_replicate {n : ℕ} {as : List α} (h : n ≠ 0) :
 -- ((List.replicate n as).flatten.head?) = as.head? := by
 -- match n, as with
@@ -22,7 +25,7 @@ theorem append_cancel_right_length {as bs bs' cs : List α}
 -- | n+1, [] => simp
 -- | n+1, h::_ => simp [replicate]
 
--- [UPSTREAMED]
+-- [UPSTREAMED] PR: https://github.com/leanprover-community/mathlib4/pull/22510
 -- theorem getLast?_flatten_replicate  {n : ℕ} {as : List α} (h : n ≠ 0) :
 -- ((List.replicate n as).flatten.getLast?) = as.getLast? := by
 --   rw [← List.head?_reverse, ← List.head?_reverse,List.reverse_flatten, List.map_replicate,
--- equational_theories/Mathlib/GroupTheory/OrderOfElement.lean
+++ equational_theories/Mathlib/GroupTheory/OrderOfElement.lean
@@ -5,8 +5,10 @@ universe u
 variable {α : Type u}
 
 -- [UPSTREAMED]: https://github.com/leanprover-community/mathlib4/pull/24972
-theorem isOfFinOrder_of_isConj [Group α] {x y : α} :
-      IsConj x y → (IsOfFinOrder x → IsOfFinOrder y) := by
-   simp only [isConj_iff, isOfFinOrder_iff_pow_eq_one]
-   refine fun ⟨u, eq⟩ ⟨n, n_gt_0, eq'⟩ ↦ ⟨n, n_gt_0, ?_⟩
-   rw [← eq, conj_pow, eq', mul_one, mul_inv_cancel]
+-- theorem isOfFinOrder_of_isConj [Group α] {x y : α} :
+--      IsConj x y → (IsOfFinOrder x → IsOfFinOrder y) := by
+--   simp only [isConj_iff, isOfFinOrder_iff_pow_eq_one]
+--   refine fun ⟨u, eq⟩ ⟨n, n_gt_0, eq'⟩ ↦ ⟨n, n_gt_0, ?_⟩
+--   rw [← eq, conj_pow, eq', mul_one, mul_inv_cancel]
+
+@[deprecated (since := "2025-05-18")] alias isOfFinOrder_of_isConj := IsConj.isOfFinOrder
