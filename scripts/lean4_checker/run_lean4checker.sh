658829b5bde8ee80fd9543684ff897b33a04355a
Alex Meiburg <timeroot.alex@gmail.com>
2026-05-07 11:38:16 -0400

    disable leanchecker

--- scripts/lean4_checker/run_lean4checker.sh
+++ scripts/lean4_checker/run_lean4checker.sh
@@ -6,12 +6,12 @@ set -e
 # https://github.com/leanprover/lean-action/blob/main/scripts/run_lean4checker.sh
 
 # Group logging using the ::group:: workflow command
-echo "::group::lean4checker Output"
-echo "Checking environment with leanchecker"
+echo "::group::leanchecker Output"
 
-# run lean4checker
-echo "Running lean4checker"
-~/.elan/bin/lake env leanchecker equational_theories
+# https://leanprover.zulipchat.com/#narrow/channel/458659-Equational/topic/v4.2E29.20bump/near/593523380
+echo "Skipping leanchecker due to excessive memory consumption for large computations(?)
+# echo "Checking environment with leanchecker"
+# ~/.elan/bin/lake env leanchecker equational_theories
 
 echo "::endgroup::"
 echo
